35270b6b05f9723ad03a76edd56882c82425441d
ejain6 <ewen.jain@gmail.com>
2024-10-16 15:59:37 +0200

    debut de la page detailsOffer.php + j'ai créé une feuille css pour tester avant d'inmpelemter

--- components/offer/detailsOffer.php
+++ components/offer/detailsOffer.php
@@ -16,11 +16,11 @@
             <label for="nom">Nom de votre offre</label>
             <input type="text" id="nom" name="nom" placeholder="Nom" required>
 
-            <label for="description">Résumé de l'offre</label>
-            <input type="textarea" id="description" name="nom" placeholder="Courte descrition, 255 caractères maximum" required>
+            <label for="resume">Résumé de l'offre</label>
+            <input type="textarea" id="resume" name="resume" placeholder="Courte descrition, 255 caractères maximum" required>
 
             <label for="description">Description de votre offre</label>
-            <input type="textarea" id="description" name="nom" placeholder="Description détaillée" required>
+            <input type="textarea" id="description" name="description" placeholder="Description détaillée" required>
 
 
         
@@ -33,16 +33,17 @@
             <p>Vous pouvez insérer jusqu'à 10 photos</p>
             <input type="file" id="photos" name="photos" required>
 
+            <div>
             <label for="nom">Catégorie</label>   
             <button type="button" value = restaurant>Restaurant</button>
             <button type="button" value = parc>Parc d'attraction</button>
             <button type="button" value = activite>Activité</button>
             <button type="button" value = spectacle>Spectacle</button>
             <button type="button" value = visite>Visite</button>
-
+            </div>
 
             <label for="tag">Tags : </label>
-            <input type="text" id="description" name="nom" required>
+            <input type="text" id="tag" name="tag">
             <button type="button" value = ajoutTag>Ajouter</button>
             <section>
                 
