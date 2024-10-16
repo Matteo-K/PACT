dec53e31ca7c2a434bea039b0e83e42ecffd25e6
BZHKylian <kylian.houedec.56@gmail.com>
2024-10-16 10:15:26 +0200

    modification du css pour le header

--- components/header.php
+++ components/header.php
@@ -1,9 +1,15 @@
-<header>
+<link rel="stylesheet" href="../style.css">
+<header theme="light">
     <div>
-        <div><a href="index.php"><img id="logo" src="./img/logo.png" title="logo du site"></a></div>
-        <form method="post" action="">
-            <input type="text">
-            <button type="submit"><img src="" title="icone de recherche"></button>
+        <div>
+            <a href="../index.php">
+                <img id="logo" src="../img/logo.png" title="logo du site">
+                <h1>PACT</h1>
+            </a>
+        </div>
+        <form method="post" action="" id="formHeader">
+            <input type="text" placeholder="Rechercher :">
+            <button type="submit"><img src="../img/icone/loupe.png" title="icone de recherche"></button>
         </form>
         <div id="auth">
             <a href="login.php"><div id="btnConn">Connexion</div></a>
