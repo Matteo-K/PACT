d13104736fbbfc91658b852d726ed3acbe6e03c1
Matteo-K <kervadecmatteo@gmail.com>
2024-10-16 09:35:54 +0200

    commit

--- components/footer.php
+++ components/footer.php
@@ -1,33 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="../style.css">
-</head>
-<body theme="light">
-  <footer>
+<link rel="stylesheet" href="../style.css">
+<footer>
+  <div>
     <div>
       <figure>
         <img src="../img/logoTripEnArvor.png" alt="logo TripEnArvor">
         <figcaption>TripEnArvor&nbsp;---&nbsp;<span>PACT</span></figcaption>
       </figure>
-      <p>Toute ressemblance avec un site web existant ou ayant existé serait purement fortuite et ne pourrait être que le fruit d'une pure coïncidence</p>
+      <p>Toute ressemblance avec un site web existant ou ayant existé serait purement fortuite et ne pourrait être que le fruit d'une pure coïncidence.</p>
     </div>
     <address>
-      <a href="mentionsLegales.php"><h4>Mentions légales</h4></a>
+      <h4><a href="mentionsLegales.php">Mentions légales</a></h4>
       <div>
         Equipe de conception / dev “ The Void - A21 “<br>
         Rue Edouard Branly, 22300 Lannion, France <br>
         ewen@jain-etudiants.univ-rennes1.com
       </div>
     </address>
-    <div>
-      <a href="planDuSite.php">Plan du site</a>
-      <p>Copyright&nbsp;&copy;&nbsp;2024 TripEnArvor</p>
-    </div>
-  </footer>
-</body>
-
-</html>
+  </div>
+  <div>
+    <a href="planDuSite.php">Plan du site</a>
+    <p>Copyright&nbsp;&copy;&nbsp;2024 TripEnArvor</p>
+  </div>
+</footer>
