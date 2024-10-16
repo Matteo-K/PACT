858d62f7aea910c058b5d420a854c78c9fd98cca
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-10-16 17:04:32 +0200

    valid

--- components/offer/hourlyOffer.php
+++ components/offer/hourlyOffer.php
@@ -1,41 +1,42 @@
 <form action="" id="hourlyOffer">
-<!-- Lundi -->
-<?php echo "Lundi"; ?>
+    <!-- Lundi -->
+    <div>
+        <?php echo "Lundi"; ?>
         <input type="checkbox" id="checkLundi" name="checkLundi" />
-<?php 
-    echo "Fermé";
+        <?php
+        echo "Fermé";
 
-    echo "Ouvert de : "
-?>
+        echo "Ouvert de : "
+            ?>
 
         <input type="time" name="horairesOuv1Lundi" id="horairesOuv1Lundi">
-    <?php echo "à" ?>
-<input type="time" name="horairesF1Lundi" id="horairesF1Lundi">
+        <?php echo "à" ?>
+        <input type="time" name="horairesF1Lundi" id="horairesF1Lundi">
 
         <input type="button" value="Ajouter un horaire" name="AjoutLundi" id="AjoutLundi">
     </div>
-<!-- Mardi -->
+    <!-- Mardi -->
     <div>
-<?php echo "Mardi"; ?>
+        <?php echo "Mardi"; ?>
         <input type="checkbox" id="checkMardi" name="checkMardi" />
-<?php 
-    echo "Fermé";
+        <?php
+        echo "Fermé";
 
-    echo "Ouvert de : "
-?>
+        echo "Ouvert de : "
+            ?>
         <input type="time" name="horairesOuv1Mardi" id="horairesOuv1Mardi">
-    <?php echo "à" ?>
-<input type="time" name="horairesF1Mardi" id="horairesF1Mardi">
+        <?php echo "à" ?>
+        <input type="time" name="horairesF1Mardi" id="horairesF1Mardi">
 
-<?php echo "et de " ?>
+        <?php echo "et de " ?>
         <input type="time" name="horairesOuv2Mardi" id="horairesOuv2Mardi">
-    <?php echo "à" ?>
+        <?php echo "à" ?>
         <input type="time" name="horairesF2Mardi" id="horairesFMardi">
         <input type="button" value="Retirer" name="Retirer" id="Retirer">
 
     </div>
 
-    
+
     <!-- Mercredi -->
 
     <div>
