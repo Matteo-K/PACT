4407a5897b3fb5eb2f4b169af661739a7948c19d
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-10-16 10:15:38 +0200

    modif

---
+++ components/offer/hourlyOffer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horaires</title>
+</head>
+<body>
+    <?php echo "Lundi"; ?>
+    <input type="checkbox" id="Lundi" name="Lundi"  />
+    <?php 
+        echo "Fermé";
+
+        echo "Ouvert de : "
+    ?>
+    
+    <?php echo "à"?>
+    <textarea name="horairesAMLundi" id="horairesAMLundi "></textarea> <!-- pas textArea -->
+    
+</body>
+</html>
