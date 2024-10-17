7f1d1917a332a5ec04d33966046a9de852d26df1
ejain6 <ewen.jain@gmail.com>
2024-10-17 14:01:06 +0200

    modifs details offre (php + css)

--- components/offer/detailsOffer.php
+++ components/offer/detailsOffer.php
@@ -4,36 +4,34 @@
 
     <div>
 
-        <label for="nom">Nom de votre offre</label>
+        <label for="nom">Nom de votre offre*</label>
         <input type="text" id="nom" name="nom" placeholder="Nom" required>
 
         <label for="resume">Résumé de l'offre</label>
-        <input type="textarea" id="resume" name="resume" placeholder="Courte descrition, 255 caractères maximum" required>
+        <textarea id="resume" name="resume" placeholder="Courte descrition, 100 caractères maximum" maxlength=99> </textarea>
 
-        <label for="description">Description de votre offre</label>
-        <input type="textarea" id="description" name="description" placeholder="Description détaillée" required>
-
-
+        <label for="description">Description de votre offre*</label>
+        <textarea id="description" name="description" placeholder="Description détaillée" maxlength=999 required> </textarea>
     
     </div>
 
 
     <div>
 
-        <label for="photos">Photos de votre offre</label>
+        <label for="photos">Photos de votre offre*</label>
         <p>Vous pouvez insérer jusqu'à 10 photos</p>
         <input type="file" id="photos" name="photos" required>
 
         <div>
-            <label for="nom">Catégorie</label>   
-            <button type="button" value = restaurant>Restaurant</button>
-            <button type="button" value = parc>Parc d'attraction</button>
-            <button type="button" value = activite>Activité</button>
-            <button type="button" value = spectacle>Spectacle</button>
-            <button type="button" value = visite>Visite</button>
+            <label for="categorie">Catégorie*</label>   
+            <input type="radio" name="categorie" id="radioRestaurant" value=restaurant><label for="radioRestaurant">Restaurant</label>
+            <input type="radio" name="categorie" id="radioParc" value=parc><label for="radioParc">Parc d'attraction</label>
+            <input type="radio" name="categorie" id="radioActivite" value=activite><label for="radioActivite">Activite</label>
+            <input type="radio" name="categorie" id="radioSpectacle" value=spectacle><label for="radioSpectacle">Spectacle</label>
+            <input type="radio" name="categorie" id="radioVisite" value=visite><label for="radioVisite">Visite</label>
         </div>
 
-        <label for="tag">Tags : </label>
+        <label for="tag">Tags associés </label>
         <input type="text" id="tag" name="tag">
         <button type="button" value = ajoutTag>Ajouter</button>
         <section>
