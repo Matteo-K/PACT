486f4e1a2abd9ac881cf5ade2f49f505efd7442a
ejain6 <ewen.jain@gmail.com>
2024-10-17 16:24:49 +0200

    modifs page et css

--- components/offer/detailsOffer.php
+++ components/offer/detailsOffer.php
@@ -8,10 +8,10 @@
         <input type="text" id="nom" name="nom" placeholder="Nom" required>
 
         <label for="resume">Résumé de l'offre</label>
-        <textarea id="resume" name="resume" placeholder="Courte descrition, 100 caractères maximum" maxlength=99> </textarea>
+        <textarea id="resume" name="resume" placeholder="Courte descrition, 100 caractères maximum" maxlength=99></textarea>
 
         <label for="description">Description de votre offre*</label>
-        <textarea id="description" name="description" placeholder="Description détaillée" maxlength=999 required> </textarea>
+        <textarea id="description" name="description" placeholder="Description détaillée, 1000 caractères maximum" maxlength=999 required></textarea>
     
     </div>
 
@@ -22,17 +22,25 @@
         <p>Vous pouvez insérer jusqu'à 10 photos</p>
         <input type="file" id="photos" name="photos" required>
 
-        <div>
-            <label for="categorie">Catégorie*</label>   
-            <input type="radio" name="categorie" id="radioRestaurant" value=restaurant><label for="radioRestaurant">Restaurant</label>
-            <input type="radio" name="categorie" id="radioParc" value=parc><label for="radioParc">Parc d'attraction</label>
-            <input type="radio" name="categorie" id="radioActivite" value=activite><label for="radioActivite">Activite</label>
-            <input type="radio" name="categorie" id="radioSpectacle" value=spectacle><label for="radioSpectacle">Spectacle</label>
-            <input type="radio" name="categorie" id="radioVisite" value=visite><label for="radioVisite">Visite</label>
+
+        <div id="affichePhotos">
+            <input type="file" id="imageInput" accept="image/*" multiple>
+            <div id="imagePreview"></div>
         </div>
 
-        <label for="tag">Tags associés </label>
-        <input type="text" id="tag" name="tag">
+        
+
+        <div>
+            <label for="categorie">Catégorie de l'offre*</label>   
+            <input type="radio" name="categorie" id="radioRestaurant" value=restaurant> <label for="radioRestaurant">Restaurant</label>
+            <input type="radio" name="categorie" id="radioParc" value=parc> <label for="radioParc">Parc d'attraction</label>
+            <input type="radio" name="categorie" id="radioActivite" value=activite> <label for="radioActivite">Activite</label>
+            <input type="radio" name="categorie" id="radioSpectacle" value=spectacle> <label for="radioSpectacle">Spectacle</label>
+            <input type="radio" name="categorie" id="radioVisite" value=visite> <label for="radioVisite">Visite</label>
+        </div>
+
+        <label for="tag">Tags supplémentaires </label>
+        <input type="text" id="tag" name="tag" placeholder="Entrez un tag décrivant votre activité / établissement">
         <button type="button" value = ajoutTag>Ajouter</button>
         <section>
             
@@ -42,8 +50,6 @@
             Vous pouvez entrer jusqu'à 6 tags
         </p>
 
-        <label for="tag">Photos de votre offre</label>
-        <input type="file" id="photos" name="photos" required>
 
     </div>
 
