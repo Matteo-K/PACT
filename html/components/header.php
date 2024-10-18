82daf35bb47cf5c0f7c985a568094d5afdd1ee35
BZHKylian <kylian.houedec.56@gmail.com>
2024-10-18 11:23:57 +0200

    connection BDD

--- html/components/header.php
+++ html/components/header.php
@@ -1,3 +1,13 @@
+<?php
+require_once "../db2.php";
+$isLoggedIn = isset($_SESSION["idUser"]);
+if($isLoggedIn){
+    $stmt = $conn->prepare("SELECT * FROM pact._utilisateur WHERE idu = ?");
+    $stmt -> execute([$_SESSION["idUser"]]);
+}
+
+?>
+
 <link rel="stylesheet" href="../style.css">
 <header theme="light">
     <div>
@@ -10,7 +20,21 @@
         <input type="text" placeholder="Rechercher :">
         <button type="submit"><img src="../img/icone/loupe.png" title="icone de recherche"></button>
     </form>
+
     <div id="auth">
-        <a href="../login.php"><div id="btnConn">Connexion</div></a>
+        <?php
+        if($isLoggedIn){
+            $photo -> $conn -> prepare("SELECT img.url FROM _photo_profil pp JOIN _image img on pp.url = img.url WHERE pp.idU = ?");
+            $photo -> execute([$_SESSION["idUser"]]);
+        ?>
+
+        <?php
+        }else{
+        ?>
+            <a href="../login.php"><div id="btnConn">Connexion</div></a>
+        <?php
+        }
+        ?>
+        
     </div>
 </header>
