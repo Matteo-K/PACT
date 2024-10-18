e749036caa9291caa5a43b5c39501138a47265b0
Matteo-K <kervadecmatteo@gmail.com>
2024-10-18 11:36:17 +0200

    set button next

--- html/enregOffer.php
+++ html/enregOffer.php
@@ -1,11 +1,17 @@
 <?php
+$page = isset($_POST['page']) ? $_POST['page'] : 1;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $page != -1) {
 
 }
 
-$page = isset($_POST['page']) ? $_POST['page'] : 1;
 
+
+if ($page == -1 || $page == 0) {
+  // redirige page d'acceuil
+} else {
+  
+}
 header("Location: manageOffer.php?page=" . $page);
 exit();
 ?>
