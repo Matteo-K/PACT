b768e9250e1578c9c3b0757e416f47b555e76ba4
BZHKylian <kylian.houedec.56@gmail.com>
2024-10-18 11:26:58 +0200

    test

--- html/db.php
+++ html/db.php
@@ -1,7 +1,7 @@
 <?php
 include('./connectionBDD/connect_params.php');
     $conn = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
-    foreach($conn->query('SELECT u.idU, u.password, m.pseudo FROM pact._membre m JOIN pact._utilisateur u ON m.idU = u.idU ', PDO::FETCH_ASSOC) as $row) {
+    foreach($conn->query('SELECT u.idU, u.password FROM pact._utilisateur u', PDO::FETCH_ASSOC) as $row) {
         echo "<pre>";
         print_r($row);
         echo "</pre>";
