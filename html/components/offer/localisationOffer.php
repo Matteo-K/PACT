fb55ac56cb65859f06d20ccc21c975013ec762e4
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-10-18 14:50:31 +0200

    ajout carte

--- html/components/offer/localisationOffer.php
+++ html/components/offer/localisationOffer.php
@@ -1,17 +1,18 @@
-<form id="localisationOffer" action="enregOffer.php" method="post">
-  <section class="sectionLoca">
-    <div>
-      <label for="Adresse">Adresse postale* :</label>
-      <input type="text" placeholder="Adresse"/>
-    </div>
-    <section>
-      <div class="codeP">
-        <label for="codepostal">Code postal* :</label>
-        <input type="text" placeholder="Code postal"/>
+  <form id="localisationOffer" action="enregOffer.php" method="post">
+    <section class="sectionLoca">
+      <div>
+        <label for="Adresse">Adresse postale* :</label>
+        <input type="text" placeholder="Adresse"/>
       </div>
-      <div class="villeL">
-        <label for="ville">Ville* :</label>
-        <input type="text" placeholder="Ville"/>
-      </div>
+      <section>
+        <div class="codeP">
+          <label for="codepostal">Code postal* :</label>
+          <input type="text" placeholder="Code postal"/>
+        </div>
+        <div class="villeL">
+          <label for="ville">Ville* :</label>
+          <input type="text" placeholder="Ville"/>
+        </div>
+      </section>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5260.469523183862!2d-3.4543869230104067!3d48.758312971318844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48122c034b5275af%3A0x355e62516b08c22f!2sIUT%20de%20Lannion%20-%20Universit%C3%A9%20de%20Rennes!5e0!3m2!1sfr!2sfr!4v1729254271589!5m2!1sfr!2sfr" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
-  </section>
