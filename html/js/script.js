01ca5f9e5936db08d31c4a52652cb3be3e0c0f35
Matteo-K <kervadecmatteo@gmail.com>
2024-10-21 11:44:18 +0200

    update hourly Representation

--- html/js/script.js
+++ html/js/script.js
@@ -53,13 +53,77 @@ try {
 // });
 
 /* Intéraction horaire */
+let counterRep = 1;
+let date_ = new Date();
+let current_date = String(
+  date_.getUTCFullYear() +
+    "-" +
+    (date_.getUTCMonth() + 1) +
+    "-" +
+    date_.getUTCDate()
+);
+/**
+ * @brief Ajout d'un div représentation à chaque click du bouton ajouter bloc
+ */
+function addDateRep() {
+  counterRep++;
+  const dateContainer = document.getElementById("Representation");
+
+  // Création d'un nouveau bloc
+  const newBlock = document.createElement("div");
+  newBlock.innerHTML = `
+        <input type="date" name="dateRepN${counterRep}" id="dateRepresentation" value="${current_date}" min="${current_date}">
+        <span class="hourly1">
+            <label for="HRepN${counterRep}_part1.1">Représentation de</label>
+            <input type="time" name="HRepN${counterRep}_part1.1" id="HRepN${counterRep}_part1.1">
+            <label for="HRepN${counterRep}_part1.2">à</label>
+            <input type="time" name="HRepN${counterRep}_part1.2" id="HRepN${counterRep}_part1.2">
+        </span>
+        <input type="button" value="Retirer" name="btnRetirerRepN${counterRep}" id="btnRetirerRepN${counterRep}" class="blueBtnOffer" onclick="removeDateRep(this)">
+    `;
+
+  // Ajout du nouveau bloc au bloc de représentation
+  dateContainer.appendChild(newBlock);
+}
+
+/**
+ * @brief Retire le bloc Représentation
+ */
+function removeDateRep(button) {
+  const rep = button.parentElement;
+  rep.remove();
+}
+
+function toggleInputs(checkbox) {
+  const timeInputs = checkbox.parentNode.querySelectorAll('input[type="time"]');
+  const buttons = checkbox.parentNode.querySelectorAll("input[type='button']");
+  if (checkbox.checked) {
+    // Désactiver les boutons et inputs time
+    buttons.forEach((button) => {
+      button.disabled = true;
+      button.classList.add("btnDisabledHourly");
+    });
+    timeInputs.forEach((input) => {
+      input.disabled = true;
+      input.value = ""; // Réinitialiser le contenu des inputs time
+    });
+  } else {
+    // Réactiver les boutons et inputs time
+    buttons.forEach((button) => {
+      button.disabled = false;
+      button.classList.remove("btnDisabledHourly");
+    });
+    timeInputs.forEach((input) => (input.disabled = false));
+  }
+}
+
 try {
   const btnsAddHourly = document.querySelectorAll(".btnAddOffer");
   const btnsRmHourly = document.querySelectorAll(".btnRmOffer");
-  const chckbxClose = document.querySelectorAll(
-    "#hourlyOffer input[type='checkbox']"
-  );
 
+  /**
+   * @brief Ajoute une horaire à la journée
+   */
   btnsAddHourly.forEach((button) => {
     button.addEventListener("click", () => {
       let nextSpan = button.nextElementSibling;
@@ -70,6 +134,9 @@ try {
     });
   });
 
+  /**
+   * @brief Retire une horaire à la journée
+   */
   btnsRmHourly.forEach((button) => {
     button.addEventListener("click", () => {
       let span = button.previousElementSibling;
@@ -82,30 +149,4 @@ try {
       span.previousElementSibling.classList.remove("hourlyHide");
     });
   });
-
-  function toggleInputs(checkbox) {
-    const timeInputs =
-      checkbox.parentNode.querySelectorAll('input[type="time"]');
-    const buttons = checkbox.parentNode.querySelectorAll(
-      "input[type='button']"
-    );
-    if (checkbox.checked) {
-      // Désactiver les boutons et inputs time
-      buttons.forEach((button) => {
-        button.disabled = true;
-        button.classList.add("btnDisabledHourly");
-      });
-      timeInputs.forEach((input) => {
-        input.disabled = true;
-        input.value = ""; // Réinitialiser le contenu des inputs time
-      });
-    } else {
-      // Réactiver les boutons et inputs time
-      buttons.forEach((button) => {
-        button.disabled = false;
-        button.classList.remove("btnDisabledHourly");
-      });
-      timeInputs.forEach((input) => (input.disabled = false));
-    }
-  }
 } catch (error) {}
