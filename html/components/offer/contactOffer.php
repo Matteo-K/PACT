8fe384333d46da9824876cb90d4e06d70b652394
Matteo-K <kervadecmatteo@gmail.com>
2024-10-21 14:57:06 +0200

    set up contact

--- html/components/offer/contactOffer.php
+++ html/components/offer/contactOffer.php
@@ -1,14 +1,24 @@
+<?php 
+  $mail = "";
+  $phone = "";
+  $DisplayNumber = true;
+  $linkWeb = "https://";
+?>
 <form id="contactOffer" action="enregOffer.php" method="post">
-  <label for="">Adresse mail de contact pour votre offre*&nbsp;:&nbsp;</label>
-  <input type="email" name="" id="" required="required" placeholder="Adresse mail">
-  <label for="">Numéro de fixe&nbsp;:&nbsp;</label>
-  <input type="tel" name="" id="" placeholder="Numéro fixe">
+  <label for="mail">Adresse mail de contact pour votre offre*&nbsp;:&nbsp;</label>
+  <input type="email" name="mail" id="mail" required="required" placeholder="Adresse mail - (exemple@mail.com)">
+  <label for="phone">Numéro de fixe&nbsp;:&nbsp;</label>
+  <input type="tel" name="phone" id="phone" placeholder="Numéro fixe">
   <div>
-    <label for="">Consentez vous à afficher votre numéro de portable sur l’offre &nbsp;?&nbsp;</label>
-    <input type="radio" name="" id="">
-    <label for="">Oui</label>
-    <input type="radio" name="" id="">
-    <label for="">Non</label>
+    <h4>Consentez vous à afficher votre numéro de portable sur l’offre &nbsp;?&nbsp;</h4>
+    <div>
+      <input type="radio" name="DisplayNumber" id="Oui" checked>
+      <label for="Oui">Oui</label>
+    </div>
+    <div>
+      <input type="radio" name="DisplayNumber" id="Non">
+      <label for="Non">Non</label>
+    </div>
   </div>
-  <label for="">Si vous avez un site web pour votre offre, vous pouvez insérer son lien ici pour qu’il apparaîsse sur l’offre :</label>
-  <input type="text" name="" id="" placeholder="Lien vers votre site web">
+  <label for="webSide">Si vous avez un site web pour votre offre, vous pouvez insérer son lien ici pour qu’il apparaîsse sur l’offre&nbsp;:&nbsp;</label>
+  <input type="text" name="webSide" id="webSide" placeholder="Lien vers votre site web" value="https://">
