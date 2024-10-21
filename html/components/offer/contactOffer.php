22d86eb4c55dfea0813d9add92db319c048be0aa
Matteo-K <kervadecmatteo@gmail.com>
2024-10-21 16:56:55 +0200

    set-up contact

--- html/components/offer/contactOffer.php
+++ html/components/offer/contactOffer.php
@@ -6,17 +6,17 @@
 ?>
 <form id="contactOffer" action="enregOffer.php" method="post">
   <label for="mail">Adresse mail de contact pour votre offre*&nbsp;:&nbsp;</label>
-  <input type="email" name="mail" id="mail" required="required" placeholder="Adresse mail - (exemple@mail.com)">
+  <input type="email" name="mail" id="mail" required="required" placeholder="Adresse mail - (exemple@mail.com)" value="<?php echo $mail; ?>">
   <label for="phone">Numéro de fixe&nbsp;:&nbsp;</label>
-  <input type="tel" name="phone" id="phone" placeholder="Numéro fixe">
+  <input type="tel" name="phone" id="phone" placeholder="Numéro fixe" value="<?php echo $phone; ?>">
   <div>
     <h4>Consentez vous à afficher votre numéro de portable sur l’offre &nbsp;?&nbsp;</h4>
     <div>
-      <input type="radio" name="DisplayNumber" id="Oui" checked>
+      <input type="radio" name="DisplayNumber" id="Oui" <?php $DisplayNumber?"checked":""?>>
       <label for="Oui">Oui</label>
     </div>
     <div>
-      <input type="radio" name="DisplayNumber" id="Non">
+      <input type="radio" name="DisplayNumber" id="Non" <?php !$DisplayNumber?"checked":""?>>
       <label for="Non">Non</label>
     </div>
   </div>
