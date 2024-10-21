b0b16ecd766335a45208f28c62481f0fb5925935
Arkaaade <antoine.guillerm@etudiant.univ-rennes.fr>
2024-10-21 16:58:40 +0200

    changement fichier

--- html/creation_compte_pro.php
+++ html/creation_compte_pro.php
@@ -8,13 +8,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="logo.png" type="image/x-icon">
+    <link rel="icon" href="img/logo.png" type="image/x-icon">
     <title>Créer un compte</title>
 </head>
 <body id ="body_creation_compte_pro" class="creation-compte">
     <aside id="asideRetour">
         <button id="retour">
-            <img src="logo.png" alt="Logo" title="Retour page précédente"/>
+            <img src="img/logo.png" alt="Logo" title="Retour page précédente"/>
             Retour
         </button>
     </aside>
