ec045788a072618cad9ba82c26d41f5db9b70e32
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-10-22 13:46:31 +0200

    affichage option specialOffer avec le Js

--- html/components/offer/details/detailsShow.php
+++ html/components/offer/details/detailsShow.php
@@ -1,24 +1,24 @@
 <!-- Spectacle -->
 
 <section id="show"> <!-- Id pour pouvoir modifer separement dans le css -->
-    <?php echo " partie show (penser a supp) "?> <br>
+
     <article> <!-- Article pour le placement -->
         <label id="ligne1">Nombre de place : </label> <!-- Label nombre de place -->
-        <input type="number" id="nb"name="nbPlaceShow" placeholder="0"> <!-- Pour le nombre de place -->
+        <input type="number" id="nb" name="nbPlaceShow" placeholder="0"> <!-- Pour le nombre de place -->
     </article>
     <br>
-    
+
     <article> <!-- Article pour le placement -->
-        
-        
-        
-        <label id="ligne1">Durée : </label> 
-        <input type="number" id="nb"name="DuréeShow" placeholder="0"> <!-- Pour entrer une durée -->
+
+
+
+        <label id="ligne1">Durée : </label>
+        <input type="number" id="nb" name="DuréeShow" placeholder="0"> <!-- Pour entrer une durée -->
     </article>
 
- 
 
-    
+
+
 
 
 </section>
