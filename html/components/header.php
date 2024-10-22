4f0fe939d40aed17254278207cf871bc8847cc2f
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-10-22 11:42:59 +0200

    header responsive

--- html/components/header.php
+++ html/components/header.php
@@ -1,17 +1,32 @@
-
 <header theme="light">
-    <div>
-        <a href="../index.php">
-            <img id="logo" src="../img/logo.png" title="logo du site">
+    <!-- Menu burger pour mobile -->
+    <div id="menuBurger">
+        <img src="./img/icone/burger-bar.png" alt="menu burger">
+    </div>
+
+    <!-- Logo du site -->
+    <div class>
+        <a href="./index.php">
+            <img id="logo" src="./img/logo.png" title="logo du site">
             <h1 id="logoText">PACT</h1>
         </a>
     </div>
+
+    <!-- Formulaire de recherche (sera déplacé en bas pour mobile) -->
     <form method="post" action="search.php" id="formHeader">
         <input type="text" placeholder="Rechercher :">
-        <button type="submit"><img src="../img/icone/loupe.png" title="icone de recherche"></button>
+        <button type="submit"><img src="./img/icone/loupe.png" title="icone de recherche"></button>
     </form>
 
+    <!-- Bouton de connexion -->
     <div id="auth">
-        <a href="../login.php"><div id="btnConn">Connexion</div></a>
+        <a href="./profile.php">
+            <div id="btnConn">Connexion</div>
+        </a>
+    </div>
+    <div id="auth2">
+        <a href="./profile.php">
+            <img id="profilePicture" src="./img/profile_picture/default.svg" alt="Photo de profil">
+        </a>
     </div>
 </header>
