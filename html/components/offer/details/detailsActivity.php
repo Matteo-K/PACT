0389b42f3280fd327e00b4fed000b160c2098b4c
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-10-22 15:25:29 +0200

    avancement CSS

--- html/components/offer/details/detailsActivity.php
+++ html/components/offer/details/detailsActivity.php
@@ -3,9 +3,11 @@
     <article> <!-- Article pour le placement des elements de gauche-->
         <p>
             <label id="labDuréeAct" name="labDuréeAct"> Durée: </label>
+            <br>
             <textarea name="duréeAct" id="txtAct" placeholder="0"></textarea>
             <label id="labHAct" name="labHAct"> H </label>
         </p>
+
         <p>
             <label id="labAgeAct" name="labAgeAct"> Age: </label>
             <textarea name="AgeAct" id="txtAct" placeholder="Age"></textarea>
