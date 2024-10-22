e6f0884adfcd762413895989ec16a196f87d62f9
Matteo-K <kervadecmatteo@gmail.com>
2024-10-22 15:19:48 +0200

    search

--- html/search.php
+++ html/search.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Recherche d'offre</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body id="search">
     <?php require_once "components/header.php" ?>
-    <body>
-        
-    </body>
+    <main>
+        <aside>
+            <h2>Tri des offres</h2>
+            <h2>Filtre</h2>
+        </aside>
+        <section>
+
+        </section>
+    </main>
 </body>
 </html>
