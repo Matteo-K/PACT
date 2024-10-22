135f50812763cd07f2e960f5dd353eb2f24d4ad0
BZHKylian <kylian.houedec.56@gmail.com>
2024-10-22 10:48:32 +0200

    modification avec verification d'erreur de connexion a la bdd

--- html/db.php
+++ html/db.php
@@ -1,5 +1,13 @@
 <?php
 include('./connectionBDD/connect_params.php');
+
+try {
     $conn = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
-    $conn = null;
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Display error message if connection fails
+    echo "Database connection failed: " . $e->getMessage();
+    exit(); // Stop further execution if the connection fails
+}
 ?>
