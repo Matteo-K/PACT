e4a59f5db01ad59884e6e77239a000b6022ba39f
Matteo-K <kervadecmatteo@gmail.com>
2024-10-23 14:52:42 +0200

    plein de modif

--- html/components/offer/selectOffer.php
+++ html/components/offer/selectOffer.php
@@ -1,8 +1,16 @@
 <?php
-$is_prive = true;
+// select * from _abonnement where idoffre = $idOffre;
+// si isset($result)?$result["nomAbonnement"]:"";
+$abonnement = "";
+// select option from _option where idoffre = $idOffre;
+$options = $res;
+$is_prive = $_SESSION["typeUser"] == "pro_prive";
 ?>
 <form id="selectOffer" action="enregOffer.php" method="post">
   <div>
+    <?php
+    if (!$is_prive) {
+    ?>
     <div>
       <h2>Offre Gratuit</h2>
       <h3>0&euro;&nbsp;/&nbsp;mois</h3>
@@ -10,10 +18,13 @@ $is_prive = true;
         <li>Réservée au public</li>
       </ul>
       <div>
-        <input type="radio" name="typeOffre" id="gratuit" value="gratuit" <?php echo $is_prive?"disabled":"checked" ?>>
+        <input type="radio" name="typeOffre" id="gratuit" value="gratuit" checked>
         <label for="gratuit">Sélectionner</label>
       </div>
     </div>
+    <?php
+    } else {
+    ?>
     <div>
       <h2>Offre Premium</h2>
       <h3>119,99&euro;&nbsp;/&nbsp;mois</h3>
@@ -23,7 +34,7 @@ $is_prive = true;
         <li>Blackliste sur 3 avis</li>
       </ul>
       <div>
-        <input type="radio" name="typeOffre" id="premium" value="premium" <?php echo !$is_prive?"disabled":"checked" ?>>
+        <input type="radio" name="typeOffre" id="premium" value="premium" <?php echo (empty($abonnement) || $abonnement == "Premium")?"checked":"" ?>>
         <label for="premium">Sélectionner</label>
       </div>
     </div>
@@ -35,19 +46,22 @@ $is_prive = true;
         <li>Saisie d’une grille tarifaire</li>
       </ul>
       <div>
-        <input type="radio" name="typeOffre" id="standard" value="standard" <?php echo !$is_prive?"disabled":"" ?>>
+        <input type="radio" name="typeOffre" id="standard" value="standard" <?php echo ($abonnement == "Basique") ?"checked":"" ?>>
         <label for="standard">Sélectionner</label>
       </div>
     </div>
+    <?php
+    }
+    ?>
   </div>
   <div>
     <div>
       <span>(&nbsp;+14.99&euro;&nbsp;)</span>
-      <input type="checkbox" name="enRelief" id="enRelief">
+      <input type="checkbox" name="enRelief" id="enRelief" <?php echo in_array('',$options)?"checked":"" ?>>
       <label for="enRelief"><span>En relief</span> : met votre offre en exergue lors de son affichage dans la liste d’offres</label></div>
     <div>
       <span>(&nbsp;+19.99&euro;&nbsp;)</span>
-      <input type="checkbox" name="aLaUne" id="aLaUne">
+      <input type="checkbox" name="aLaUne" id="aLaUne" <?php echo in_array('',$options)?"checked":"" ?>>
       <label for="aLaUne"><span>À la une</span> : met votre offre sur la page d’accueil du site</label>
     </div>
     <p>Attention ! Vous ne pouvez pas changer d’offre une fois séléctionée.</p>
