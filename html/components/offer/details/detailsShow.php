97d62b5d1283989b6df6c36576f089d78bad6d29
ejain6 <ewen.jain@gmail.com>
2024-10-23 14:41:57 +0200

    modifs mon php + java + css + un peu pages benj

--- html/components/offer/details/detailsShow.php
+++ html/components/offer/details/detailsShow.php
@@ -3,8 +3,8 @@
 <section id="show"> <!-- Id pour pouvoir modifer separement dans le css -->
 
     <article> <!-- Article pour le placement -->
-        <label id="ligne1">Nombre de place : </label> <!-- Label nombre de place -->
-        <input type="number" id="nb" name="nbPlaceShow" placeholder="0"> <!-- Pour le nombre de place -->
+        <label class="ligne1">Nombre de place : </label> <!-- Label nombre de place -->
+        <input type="number" 2class="nb" name="nbPlaceShow" placeholder="0"> <!-- Pour le nombre de place -->
     </article>
     <br>
 
@@ -12,8 +12,8 @@
 
 
 
-        <label id="ligne1">Durée : </label>
-        <input type="number" id="nb" name="DuréeShow" placeholder="0"> <!-- Pour entrer une durée -->
+        <label class="ligne1">Durée : </label>
+        <input type="number" class="nb" name="DuréeShow" placeholder="0"> <!-- Pour entrer une durée -->
     </article>
 
 
