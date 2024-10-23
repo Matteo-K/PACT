652f12b6f5fc0598831991d7615d46b29850248c
BZHKylian <kylian.houedec.56@gmail.com>
2024-10-23 16:09:46 +0200

    test si images de l'offre s'affiche

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -10,12 +10,11 @@
 
     $idOffre = $_GET["idoffre"];
 
-    // Prepare the SQL statement with a placeholder
+    // Prepare the SQL statement with placeholders
     $stmt = $conn->prepare("SELECT * FROM pact.parcs_attractions WHERE idoffre = '$idOffre'");
     $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // Check if a result was found
     if (!$result) {
         $stmt = $conn->prepare("SELECT * FROM pact.restaurants WHERE idoffre = '$idOffre'");
         $stmt->execute();
@@ -35,14 +34,10 @@
                     $stmt = $conn->prepare("SELECT * FROM pact.visites WHERE idoffre = '$idOffre'");
                     $stmt->execute();
                     $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                    
                 }
             }
         }
-            
     }
-
-    print_r($result);
 ?>
 
 <!DOCTYPE html>
@@ -53,16 +48,14 @@
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="style.css">
-    <title><?php echo $result["nom_offre"]?> </title>
+    <title><?php echo $result["nom_offre"] ?> </title>
 </head>
 <body>
-    <?php
-        require_once "components/headerTest.php";
-    ?>
+    <?php require_once "components/headerTest.php"; ?>
     <script src="js/setColor.js"></script>
     
     <main class="mainOffer">
-        <h2 id="titleOffer"><?php echo $result["nom_offre"]?> </h2>
+        <h2 id="titleOffer"><?php echo $result["nom_offre"] ?> </h2>
         <div>
             <!-- foreach ici !-->
             <a class="tag" href="search.php?search=parc">Parc d'attraction</a>
@@ -78,30 +71,28 @@
             <a href="https://www.levillagegaulois.org/">https://www.levillagegaulois.org/</a>
         </div>
         <?php
-            $stmt = $conn->prepare("SELECT * FROM pact._illustre WHERE idoffre = '$idOffre'");
+            $stmt = $conn->prepare("SELECT * FROM pact._illustre WHERE idoffre = '$idOffre' ORDER BY url ASC");
             $stmt->execute();
-            $photos = $stmt->fetch(PDO::FETCH_ASSOC);
+            $photos = $stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
         <div class="swiper-container">
             <div class="swiper mySwiper">
                 <div class="swiper-wrapper">
-                    <?php
-                        foreach($photos as $picture){
-                    ?>
-                            <div class="swiper-slide">
-                                <img src="<?php $picture["url"] ?>"/>
-                            </div>
-                    <?php
-                        }
-                    ?>
+                <?php
+                    foreach ($photos as $picture) {
+                ?>
+                        <div class="swiper-slide">
+                            <img src="<?php echo $picture['url']; ?>" />
+                        </div>
+                <?php
+                    }
+                ?>
                 </div>
-                
             </div>
 
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
         </div>
-        
 
         <div thumbsSlider="" class="swiper myThumbSlider">
             <div class="swiper-wrapper">
@@ -122,9 +113,10 @@
                 </div>
             </div>
         </div>
+
         <!-- Swiper JS -->
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
+        
         <!-- Initialize Swiper -->
         <script>
         var swiper = new Swiper(".myThumbSlider", {
@@ -149,9 +141,7 @@
             },
         });
         </script>
-
     </main>
-    <?php require_once "components/footer.php" ?>
-    
+    <?php require_once "components/footer.php"; ?>
 </body>
 </html>
