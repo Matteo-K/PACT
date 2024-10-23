b9a376a0f19dc6ca3f391c29f5af02a8883ff9d0
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-10-23 11:12:54 +0200

    tkt frere

--- html/search.php
+++ html/search.php
@@ -102,7 +102,7 @@ $urlImg = $img->fetchAll(PDO::FETCH_ASSOC);
                     foreach ($urlImg as $array) {
                         print_r($array);
                         echo $array['url'];
-                        ?><a href="<?php $array['url'] ?>"></a><?php
+                        ?><img src="<?php echo $array['url'] ?>" alt=""><?php
                     }
                     ?>
                 </ul>
