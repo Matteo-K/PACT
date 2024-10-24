9b5e9188066ea81e8cf7b7d7d234f93d1f1da3b3
Matteo-K <kervadecmatteo@gmail.com>
2024-10-24 16:26:14 +0200

    Je sais plus ce que j'ai fais

--- html/enregOffer.php
+++ html/enregOffer.php
@@ -1,7 +1,7 @@
 <?php
 $pageDirection = $_POST['pageCurrent'] ?? 1;
-$idOffre = $_POST["idOffre"];
-$idUser = $_POST["idUser"];
+$idOffre = $_POST["idOffre"] ?? "";
+$idUser = $_POST["idUser"] ?? "";
 
 session_start();
 require_once 'db.php';
@@ -260,6 +260,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['pageBefore'])) {
                         $stmt = $conn->prepare("SELECT * FROM pact._horairemidi WHERE idoffre=? AND jour=?");
                         $stmt->execute([$idOffre, $jour]);
                         $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
                         if ($result !== false) {
                           // si existe déjà, on modifie
                           $stmt = $conn->prepare("UPDATE pact._horairemidi SET heureouverture=?, heurefermeture=? where idoffre=? and jour=?");
