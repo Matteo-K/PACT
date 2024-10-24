8ddfb3a84b2c52b139ebdb37d658d7d9da61c508
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-10-24 10:54:09 +0200

    ajout radio button details activity

--- html/components/offer/details/detailsActivity.php
+++ html/components/offer/details/detailsActivity.php
@@ -12,10 +12,25 @@
             <label class="labAgeAct" name="labAgeAct"> Age: </label>
             <textarea name="AgeAct" class="txtAct" placeholder="Age"></textarea>
             <label class="labAnsAct" name="labAgeAct"> Ans </label>
-
         </p>
 
+        <div>
+            <label>Accessibilité</label> <!-- Label Accessibilité -->
+            <br>
 
+
+            <input type="radio" id="radioButtonAccesPmrRequis" name="Accessibilité" value="Accès personne handicapées"
+                checked>
+            <!-- Bouton radio pour le choix de l'accesibilite PMR il est lier avec le 2eme et est selectionner par defaut -->
+            <label for="radioButtonAccesPmrRequis"> Accès personne handicapées</label>
+            <!-- Label associé au bouton radio -->
+            <br>
+            <input type="radio" id="pasAcces" name="Accessibilité"
+                value="Pas d’accès personne handicapées"> <!-- 2eme bouton radio liés au 1er via l'id -->
+            <label for="pasAcces"> Pas d’accès personne handicapées </label>
+            <!-- Label du 2eme bouton radio -->
+            <br>
+        </div>
 
 
 
