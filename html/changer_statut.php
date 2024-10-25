97d6ca9ddb79467da8ac94f2e20a47d6b6abac34
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-10-25 11:11:03 +0200

    petit detail

--- html/changer_statut.php
+++ html/changer_statut.php
@@ -1,6 +1,4 @@
 <?php
-// changer_statut.php
-session_start();
 // Connexion à la base de données
 // Assure-toi de remplacer les valeurs par les tiennes
 require_once "config.php";
@@ -9,7 +7,7 @@ $offreId = $_POST['offre_id'];
 $nouveauStatut = $_POST['nouveau_statut'];
 
 // Mettre à jour le statut de l'offre
-$stmt = $conn->prepare("UPDATE pact._offres SET statut =$nouveauStatut WHERE idoffre =$offreId");
+$stmt = $conn->prepare("UPDATE pact._offre SET statut =$nouveauStatut WHERE idoffre =$offreId");
 $stmt->execute();
 
 // Rediriger vers la page précédente ou une autre page
