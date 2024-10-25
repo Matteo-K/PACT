2e656e4716ac9f3354db3be99d7d21778b3da791
Arkaaade <antoine.guillerm@etudiant.univ-rennes.fr>
2024-10-25 15:01:33 +0200

    test

--- html/components/offer/previewOffer.php
+++ html/components/offer/previewOffer.php
@@ -259,3 +259,32 @@ $photos = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     }
                 ?>
     </section>
+
+    script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYU5lrDiXzchFgSAijLbonudgJaCfXrRE&callback=initMap" async defer></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <!-- Initialize Swiper -->
+    <script>
+        var swiper = new Swiper(".myThumbSliderPreview", {
+            loop: true,
+            spaceBetween: 10,
+            slidesPerView: 4,
+            freeMode: true,
+            watchSlidesProgress: true,
+        });
+        var swiper2 = new Swiper(".mySwiperPreview", {
+            loop: true,
+            autoplay: {
+                delay: 5000,
+            },
+            spaceBetween: 10,
+            navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            },
+            thumbs: {
+            swiper: swiper,
+            },
+        });
+    </script>
