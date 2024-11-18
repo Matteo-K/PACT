3540a26ee43e6eef7f399f6526730fd5f8cf6f87
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-11-18 15:26:35 +0100

    mdod

--- html/components/offer/details/detailsShow.php
+++ html/components/offer/details/detailsShow.php
@@ -1,16 +1,21 @@
 <!-- Spectacle -->
 
 <section id="show"> <!-- Id pour pouvoir modifer separement dans le css -->
+    <article>
+        <div>
+            <label class="ligne1">Nombre de place : </label> <!-- Label nombre de place -->
+            <input type="number" 2class="nb" name="nbPlaceShow" placeholder="0"> <!-- Pour le nombre de place -->
+        </div>
 
-    <div>
-        <label class="ligne1">Nombre de place : </label> <!-- Label nombre de place -->
-        <input type="number" 2class="nb" name="nbPlaceShow" placeholder="0"> <!-- Pour le nombre de place -->
-    </div>
-    <div>
-        <label class="ligne1">Durée : </label>
-        <input type="number" class="nb" name="DuréeShow" placeholder="0"> <!-- Pour entrer une durée -->
-        <?php echo "minutes" ?>
-    </div>
+
+
+        <div>
+            <label class="ligne1">Durée : </label>
+            <input type="number" class="nb" name="DuréeShow" placeholder="0"> <!-- Pour entrer une durée -->
+            <?php echo "minutes" ?>
+        </div>
+
+    </article>
     <div>
         <label class="ligne1">Prix minimum : </label>
         <input type="number" class="nb" name="PrixMinShow" placeholder="0"> <!-- Pour entrer un prix minimum -->
