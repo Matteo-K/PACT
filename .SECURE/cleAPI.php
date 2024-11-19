daecac1deba5652754da89cdef3fa0a63cb074d2
BZHKylian <kylian.houedec.56@gmail.com>
2024-11-19 17:39:16 +0100

    detailOffer fonctionnelle

--- .SECURE/cleAPI.php
+++ .SECURE/cleAPI.php
@@ -1,13 +1,18 @@
 <?php
-print(__DIR__);
-require_once __DIR__ . "/../html/vendor";
-    // Définir le chemin du fichier .env
-$envPath = __DIR__ ;
+header('Content-Type: application/json');
 
-// Charger le fichier .env
-$dotenv = Dotenv\Dotenv::createImmutable($envPath);
-$dotenv->load();
+$cleAPI = 'votre_clé_api'; // Clé API stockée en sécurité côté serveur
+$endpoint = 'https://maps.googleapis.com/maps/api/directions/json';
 
-// Utiliser la clé API
-$googleMapsApiKey = $_ENV['GOOGLE_MAPS_API_KEY'];
-?>
+// Récupérer les paramètres passés depuis le client
+$params = $_GET;
+$params['key'] = $apiKey; // Ajouter la clé API côté serveur
+
+// Construire l'URL
+$url = $endpoint . '?' . http_build_query($params);
+
+// Appeler l'API Google Maps
+$response = file_get_contents($url);
+
+// Retourner la réponse au client
+echo $response;
