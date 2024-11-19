c7aa4431a819d9b19fafb40e1a124be06c242f45
BZHKylian <kylian.houedec.56@gmail.com>
2024-11-19 17:40:49 +0100

    test

--- .SECURE/cleAPI.php
+++ .SECURE/cleAPI.php
@@ -1,18 +1,4 @@
 <?php
-header('Content-Type: application/json');
 
 $cleAPI = 'votre_clé_api'; // Clé API stockée en sécurité côté serveur
-$endpoint = 'https://maps.googleapis.com/maps/api/directions/json';
 
-// Récupérer les paramètres passés depuis le client
-$params = $_GET;
-$params['key'] = $apiKey; // Ajouter la clé API côté serveur
-
-// Construire l'URL
-$url = $endpoint . '?' . http_build_query($params);
-
-// Appeler l'API Google Maps
-$response = file_get_contents($url);
-
-// Retourner la réponse au client
-echo $response;
