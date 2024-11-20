432678cdef38564fbaf1f4baeb67cd8fd7fb4d67
Arkaaade <antoine.guillerm@etudiant.univ-rennes.fr>
2024-11-20 17:27:00 +0100

    yah

--- html/accountPro.php
+++ html/accountPro.php
@@ -153,7 +153,7 @@
                 <label for="ville">Ville*:</label>
                 
                 <!-- Saisi du code postale -->
-                <input type="text" placeholder = "75000" id="code" name="code" required>
+                <input type="text" placeholder = "29000" id="code" name="code" required>
 
                 <!-- Saisi de la ville -->
                 <input type="text" placeholder = "Paris" id="ville" name="ville" required>
