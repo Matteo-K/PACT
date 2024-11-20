3ce7a2d47988275a1d4ada1371aaae92dc89c578
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-20 16:30:56 +0100

    listimages

--- html/search.php
+++ html/search.php
@@ -303,7 +303,7 @@ $currentTime = new DateTime(date('H:i')); // ex: 14:30
                     $nomOffre = $offre['nom'];
                     $resume= $offre['resume'];
                     $noteAvg = "Non noté";
-                    print_r($offre['listeimages']);
+                    print_r($offre['listimage']);
     
                     // Requête pour récupérer l'image de l'offre
                     $img = $conn->prepare("SELECT * FROM pact._illustre WHERE idoffre = $idOffre ORDER BY url ASC");
