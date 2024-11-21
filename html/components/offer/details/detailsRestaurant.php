90ad44f018419814df701ab63bb3473dd17200c2
Benjibzh <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-11-21 17:17:31 +0100

    annuler

--- html/components/offer/details/detailsRestaurant.php
+++ html/components/offer/details/detailsRestaurant.php
@@ -1,4 +1,24 @@
+<!-- Restaurant -->
+<?php
+// Initialisation des données à vide
+$gamme = [
+  "€" => true,
+  "€€" => false,
+  "€€€" => false
+];
 
+// Si le restaurant était déjà existante, on récupère les données
+if ($categorie["_restauration"]) {
+  $stmt = $conn->prepare("SELECT * from pact._restauration where idoffre=?");
+  $stmt->execute([$idOffre]);
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  if ($result) {
+    $gamme["€"] = false;
+    $gamme[$result["gammedeprix"]] = true;
+  }
+}
+
+?>
 <section id="restaurant">
   <h4>Gamme de prix : </h4>
   <input type="radio" name="gamme_prix" id="€" value="€" <?php echo $gamme["€"] ? "checked" : "" ?>>
@@ -8,20 +28,3 @@
   <input type="radio" name="gamme_prix" id="€€€" value="€€€" <?php echo $gamme["€€€"] ? "checked" : "" ?>>
   <label for="€€€">&euro;&euro;&euro; (menu à plus de 40€)</label>
 </section>
-
-<section id="restaurant">
-  <h4>Gamme de prix :</h4>
-  <label for="€">
-    <input type="radio" name="gamme_prix" id="€" value="€" <?php echo $gamme["€"] ? "checked" : "" ?>>
-    &euro; (menu à moins de 25€)
-  </label>
-  <label for="€€">
-    <input type="radio" name="gamme_prix" id="€€" value="€€" <?php echo $gamme["€€"] ? "checked" : "" ?>>
-    &euro;&euro; (menu de 25€ à 40€)
-  </label>
-  <label for="€€€">
-    <input type="radio" name="gamme_prix" id="€€€" value="€€€" <?php echo $gamme["€€€"] ? "checked" : "" ?>>
-    &euro;&euro;&euro; (menu à plus de 40€)
-  </label>
-</section>
-``
