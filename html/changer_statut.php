4d1b2ea460dae23b328e3db180a421477e3c9805
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-23 18:19:22 +0100

    ajustement de la mise en ligne (traite le cas ou la mise hors ligne etait il y a moins de 24heures)

--- html/changer_statut.php
+++ html/changer_statut.php
@@ -14,10 +14,31 @@ $stmt->bindParam(':id', $offreId);
 $stmt->execute();
 
 if ($nouveauStatut=='actif') {
-    $ajst = $conn->prepare("INSERT INTO pact._historiqueStatut(idoffre,datelancement,dureeenligne) VALUES ($offreId,CURRENT_DATE,NULL)");
+    $ajst = $conn->prepare("SELECT idstatut,datelancement,dureeenligne FROM pact._historiqueStatut where idoffre=$offreId ORDER BY datelancement DESC");
     $ajst->execute();
+    $resultat = $ajst->fetchAll(PDO::FETCH_ASSOC);
+
+    $idstatut=$resultat[0]['idstatut'];
+
+    $dateLancement = $resultat[0]['datelancement'];
+    $dureeEnLigne = $resultat[0]['dureeenligne'];
+
+    $dateLancementObj = new DateTime($dateLancement);
+
+    $dateLancementObj->modify("+$dureeEnLigne days");
+
+    $currentDate = new DateTime();
+
+    if ($dateLancementObj->format('Y-m-d') == $currentDate->format('Y-m-d')) {
+        $ajst = $conn->prepare("UPDATE pact._historiqueStatut SET dureeenligne = NULL WHERE idstatut = $idstatut");
+        $ajst->execute();
+
+    }else {
+        $ajst = $conn->prepare("INSERT INTO pact._historiqueStatut(idoffre,datelancement,dureeenligne) VALUES ($offreId,CURRENT_DATE,NULL)");
+        $ajst->execute();
+    }
 }else {
-    $ajst = $conn->prepare("UPDATE pact._historiqueStatut SET dureeenligne = (CURRENT_DATE - datelancement) WHERE idoffre = $offreId AND dureeenligne IS NULL");
+    $ajst = $conn->prepare("UPDATE pact._historiqueStatut SET dureeenligne = (CURRENT_DATE - datelancement)+1 WHERE idoffre = $offreId AND dureeenligne IS NULL");
     $ajst->execute();
 }
 
