829eff0d3bce184f1f3b5bd1f12008d44ebc4d02
Matteo-K <kervadecmatteo@gmail.com>
2024-11-25 16:56:10 +0100

    correction affichage

--- html/class/ArrayOffer.php
+++ html/class/ArrayOffer.php
@@ -131,7 +131,7 @@ class ArrayOffer {
 
   public function displayArrayCard($idUser_, $typeUser_, $elementStart_, $nbElement_) {
     $array = $this->pagination($idUser_, $typeUser_, $elementStart_, $nbElement_);
-    if (count($array > 0)) {
+    if (count($array) > 0) {
       foreach ($array as $key => $elem) {
         if ($typeUser_ == "pro_public" || $typeUser_ == "pro_prive") {
           $elem->displayCardOfferPro();
