fee6ae31b68da92a32a815c46c4c51d68bb32de2
Matteo-K <kervadecmatteo@gmail.com>
2024-11-25 16:23:46 +0100

    test class

--- html/class/ArrayOffer.php
+++ html/class/ArrayOffer.php
@@ -129,7 +129,8 @@ class ArrayOffer {
   // TODO
   public function displayArrayCard($idUser_, $typeUser_, $elementStart_, $nbElement_) {
     $array = $this->pagination($idUser_, $typeUser_, $elementStart_, $nbElement_);
-    
+    var_dump($array);
+
     foreach ($array as $key => $elem) {
       echo $key . " " . $typeUser_."\n";
       if ($typeUser_ == "pro_public" || $typeUser_ == "pro_prive") {
