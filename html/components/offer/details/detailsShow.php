ea315af3709ef7d9e0087f10a95bea1dabdeaa1b
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-11-25 16:09:54 +0100

    hu

--- html/components/offer/details/detailsShow.php
+++ html/components/offer/details/detailsShow.php
@@ -57,7 +57,7 @@ if ($categorie["_spectacle"]) {
                 
             
 
-                <label class="ligne2">En heures : </label>
+                <label class="ligne4">En heures : </label>
                 <input type="time" class="nb" name="nbMinutesHeure" placeholder="0">
                 <label class="ligne3">h</label>
             </div>
