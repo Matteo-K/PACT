4413cb99e4f6f9916bdcb3668817e083228ea796
Matteo-K <kervadecmatteo@gmail.com>
2024-11-25 16:10:49 +0100

    test class

--- html/ClassTest.php
+++ html/ClassTest.php
@@ -2,7 +2,7 @@
 require_once "config.php";
 require_once "class/ArrayOffer.php";
 
-$arTout = new ArrayOffer(3);
+$arTout = new ArrayOffer();
 
 $arTout->displayArrayCard(18, 'passeur', 0, 20);
 print_r($arTout->getArray());
--- html/class/ArrayOffer.php
+++ html/class/ArrayOffer.php
@@ -133,13 +133,13 @@ class ArrayOffer {
   // TODO
   public function displayArrayCard($idUser_, $typeUser_, $elementStart_ , $nbElement_) {
     $array = $this->pagination($idUser_, $typeUser_, $elementStart_ , $nbElement_);
-    if (($typeUser_ == "pro_public" || $typeUser_ == "pro_prive")) {
-      foreach ($array as $key => $elem) {
+    foreach ($array as $key => $elem) {
+      if (($typeUser_ == "pro_public" || $typeUser_ == "pro_prive")) {
         $elem->displayCardOfferPro();
-      }
-    } else {
-      foreach ($array as $key => $elem) {
-        $elem->displayCardOffer();
+      } else {
+        foreach ($array as $key => $elem) {
+          $elem->displayCardOffer();
+        }
       }
     }
   }
