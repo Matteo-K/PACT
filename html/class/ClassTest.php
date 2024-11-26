61482ee3dab35b2d381a1a95c18b0ada4687ba21
Matteo-K <kervadecmatteo@gmail.com>
2024-11-26 10:39:10 +0100

    locallisation

--- html/class/ClassTest.php
+++ html/class/ClassTest.php
@@ -3,7 +3,7 @@ require_once __DIR__."/../config.php";
 
 $ar = new ArrayOffer();
 
-$array = $ar->recherche(15,"paster","Restaurant");
+$array = $ar->recherche(15,"paster","1 rue");
 
 $ar->displayArrayCard($array, "paster", 0 , 100);
 
