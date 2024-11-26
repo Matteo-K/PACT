4c4b3d2a4453a91312085fa99d5de9d01f3242ad
BZHKylian <kylian.houedec.56@gmail.com>
2024-11-27 00:42:33 +0100

    voir l'erreur dans le tableau

--- html/components/avis/avisMembre.php
+++ html/components/avis/avisMembre.php
@@ -14,7 +14,7 @@ $stmt =$conn->prepare("SELECT * from pact.proprive where idu = ?");
 $stmt->execute([$avis[0]['idpro']]);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($result[0]);
+print_r($result);
 
 foreach ($avis as $a) {
 ?>
