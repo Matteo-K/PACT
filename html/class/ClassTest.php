b36a9cd8d4fe7fd9af62c431368d7a8ebe75a497
Matteo-K <kervadecmatteo@gmail.com>
2024-11-26 10:19:30 +0100

    reto

--- html/class/ClassTest.php
+++ html/class/ClassTest.php
@@ -3,7 +3,7 @@ require_once __DIR__."/../config.php";
 
 $ar = new ArrayOffer();
 
-$array = $ar->recherche(15,"paster","cultu");
+$array = $ar->recherche(15,"paster","Restaurant");
 
 $ar->displayArrayCard($array, "paster", 0 , 100);
 
