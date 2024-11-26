4d5f1e0ff8a57df88f96379f2f422dc4427dd169
Matteo-K <kervadecmatteo@gmail.com>
2024-11-26 21:39:33 +0100

    tempo

--- html/class/Offer.php
+++ html/class/Offer.php
@@ -141,7 +141,7 @@ class Offer {
     $this->horaireSoir = $horaireSoir_;
     $this->mail = empty($mail_) ? "adresse@mail.com" : $mail_;
     $this->telephone = $telephone_;
-    $this->urlsite = $urlsite_;
+    //$this->urlsite = $urlsite_;
     $this->dateCreation = $dateCreation_;
   }
 
