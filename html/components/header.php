bb73fb3769778560d4f416cca75847036bace28e
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-26 11:17:57 +0100

    ok

--- html/components/header.php
+++ html/components/header.php
@@ -45,8 +45,8 @@
             <img id="profilePic" src="<?php echo $user["url"] ?>" title="Photo de profil utilisateur">
 
             <!-- Menu caché intégré dans le header -->
-            <div id="profileMenu">
-                <div>
+            <div class="menuHeader">
+                <div id="profileMenu">
                     <span id="backButton">< Retour</span>
                     <figure id="imagProfil">
                         <img src="<?php echo $user["url"] ?>" title="photo de profil utilisateur" id="menuProfilePic">
