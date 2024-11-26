8c81bad4cd2dbb6d85f6cb8f2c18e7909b664ff8
Arkaaade <antoine.guillerm@etudiant.univ-rennes.fr>
2024-11-26 11:17:49 +0100

    test

--- html/accountMember.php
+++ html/accountMember.php
@@ -69,6 +69,7 @@
         // Si des erreurs ont été trouvées, ne pas continuer avec l'insertion
         if (!empty($errors)) {
             $_SESSION['errors'] = $errors;
+            header('Location: accountPro.php');
         }
 
 
