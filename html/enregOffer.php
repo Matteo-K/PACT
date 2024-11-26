2ab80d2fa1f4353ca975e040943775170deee493
ejain6 <ewen.jain@gmail.com>
2024-11-26 17:58:15 +0100

    pb résolu ??????

--- html/enregOffer.php
+++ html/enregOffer.php
@@ -121,7 +121,7 @@ if (isset($_POST['pageBefore'])) {
 
         //On déplace les anciennes images conservées vers un dossier temporaire
         foreach ($anciennesImagesRestantes as $num => &$lien) {
-          move_uploaded_file($lien, $dossierTemp . $num . "." . pathinfo($lien)['extension']);
+          rename($lien, $dossierTemp . $num . "." . pathinfo($lien)['extension']);
           $lien = $dossierTemp . $num . "." . pathinfo($lien)['extension'];
         }
 
