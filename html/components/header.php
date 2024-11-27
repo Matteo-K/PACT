648c237e23565fa4408be70668f7d86a70ca3af3
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-27 14:34:14 +0100

    ajout de l'année et modif nom facture

--- html/components/header.php
+++ html/components/header.php
@@ -129,6 +129,7 @@
                                                         <form action="bill/download.php" method="post">
                                                             <input type="hidden" name="idOffre" value="<?php echo $value['idoffre']; ?>">
                                                             <input type="hidden" name="mois" value="<?php echo $moisFrancais; ?>">
+                                                            <input type="hidden" name="annee" value="<?php echo $annee; ?>">
                                                             <input type="hidden" name="date" value="<?php echo $value2; ?>">
                                                             <button class="modifierBut" type="submit">Télécharger</button>
                                                         </form>
