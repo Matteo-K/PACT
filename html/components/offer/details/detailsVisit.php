8c2ed12ad84a387576ec06acf7c89467b54cdc60
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-11-27 10:10:52 +0100

    hgf

--- html/components/offer/details/detailsVisit.php
+++ html/components/offer/details/detailsVisit.php
@@ -103,6 +103,7 @@ if ($categorie["_visite"]) {
 
         <?php
         // Connexion à la base de données (MySQL avec PDO)
+        $host = 'localhost';
         $dbname = 'plpgsql'; // Remplacez par le nom de votre base de données
         $username = 'admin1'; // Remplacez par votre utilisateur
         $password = 'motdepasse1'; // Remplacez par votre mot de passe
