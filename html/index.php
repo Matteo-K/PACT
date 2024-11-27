8573813be66a59f3c141657fc24c513c530f256c
Matteo-K <kervadecmatteo@gmail.com>
2024-11-27 11:36:45 +0100

    ajout du style

--- html/index.php
+++ html/index.php
@@ -6,6 +6,7 @@ require_once "config.php";
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>PACT</title>
 </head>
 <body id="index">
