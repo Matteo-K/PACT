c7bc30f6c7da1961c938f4c5e9044c8e082ec0db
Matteo-K <kervadecmatteo@gmail.com>
2024-11-27 20:51:17 +0100

    test flex

--- html/components/cardALaUnePro.php
+++ html/components/cardALaUnePro.php
@@ -5,44 +5,46 @@
     <img src="<?php echo $urlImg; ?>" alt=<?php echo $alt; ?>>
     <figcaption>
       <div>
-        <div class="detailsCardOffer">
-          <h3><?php echo $nomOffre ?></h3>
-          <div class="noteALaUne">
-            <span class="blcStarALaUne">
-              <?php  
-              $etoilesPleines = floor($noteAvg); // Nombre entier d'étoiles pleines
-              $reste = $noteAvg - $etoilesPleines;
-              // Étoiles pleines
-              for ($i = 1; $i <= $etoilesPleines; $i++) {
-                echo '<div class="star pleine"></div>';
-              }
-              // Étoile partielle
-              if ($reste > 0) {
-                $pourcentageRempli = $reste * 100; // Pourcentage rempli
-                echo '<div class="star partielle" style="--pourcentage: ' . $pourcentageRempli . '%;"></div>';
-              }
-              // Étoiles vides
-              for ($i = $etoilesPleines + ($reste > 0 ? 1 : 0); $i < 5; $i++) {
-                echo '<div class="star vide"></div>';
-              }
-              ?>
-            </span>
-            <span><?php echo $noteAvg ?>/5</span>
-            <span>(<?php echo $nbNote ?> avis)</span>
+        <div class="hrlBlocIndex">
+          <div class="detailsCardOffer">
+            <h3><?php echo $nomOffre ?></h3>
+            <div class="noteALaUne">
+              <span class="blcStarALaUne">
+                <?php  
+                $etoilesPleines = floor($noteAvg); // Nombre entier d'étoiles pleines
+                $reste = $noteAvg - $etoilesPleines;
+                // Étoiles pleines
+                for ($i = 1; $i <= $etoilesPleines; $i++) {
+                  echo '<div class="star pleine"></div>';
+                }
+                // Étoile partielle
+                if ($reste > 0) {
+                  $pourcentageRempli = $reste * 100; // Pourcentage rempli
+                  echo '<div class="star partielle" style="--pourcentage: ' . $pourcentageRempli . '%;"></div>';
+                }
+                // Étoiles vides
+                for ($i = $etoilesPleines + ($reste > 0 ? 1 : 0); $i < 5; $i++) {
+                  echo '<div class="star vide"></div>';
+                }
+                ?>
+              </span>
+              <span><?php echo $noteAvg ?>/5</span>
+              <span>(<?php echo $nbNote ?> avis)</span>
+            </div>
+            <adresse><?php echo $ville ?>, <?php echo $codePostal ?></adresse>
           </div>
-          <adresse><?php echo $ville ?>, <?php echo $codePostal ?></adresse>
-        </div>
-        <div class="dataOffre">
-          <span class="StatutAffiche <?php echo $statut=='actif'?"":"horslgnOffre";?>">
-              <?php echo $statut=='actif'?"En-Ligne":"Hors-Ligne";?>
-          </span>
-          <span class="abonnement">
-              <?php echo $abonnement ?>
-          </span>
-          <div>
-            <?php foreach ($options as $option) { ?>
-              <span><?php echo $option ?></span>
-            <?php } ?>
+          <div class="dataOffre">
+            <span class="StatutAffiche <?php echo $statut=='actif'?"":"horslgnOffre";?>">
+                <?php echo $statut=='actif'?"En-Ligne":"Hors-Ligne";?>
+            </span>
+            <span class="abonnement">
+                <?php echo $abonnement ?>
+            </span>
+            <div>
+              <?php foreach ($options as $option) { ?>
+                <span><?php echo $option ?></span>
+              <?php } ?>
+            </div>
           </div>
         </div>
         <div class="blcTag">
@@ -53,6 +55,7 @@
             <?php } ?>
           </div>
         </div>
+      </div>
     </figcaption>
   </figure>
 </form>
