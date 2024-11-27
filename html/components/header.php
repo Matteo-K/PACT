46c63c810a4d385f96e4df55d343d56b27cb4c4a
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-27 13:42:25 +0100

    test

--- html/components/header.php
+++ html/components/header.php
@@ -127,7 +127,7 @@
                                                         </form>
                                                         <form action="bill/download.php" method="post">
                                                             <input type="hidden" name="idOffre" value="<?php echo $value['idoffre']; ?>">
-                                                            <input type="hidden" name="mois" value="<?php echo $moisEnFrancais; ?>">
+                                                            <input type="hidden" name="mois" value=<?php echo $moisEnFrancais; ?>>
                                                             <input type="hidden" name="date" value="<?php echo $value2; ?>">
                                                             <button class="modifierBut" type="submit">Télécharger</button>
                                                         </form>
