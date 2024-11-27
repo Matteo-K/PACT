7d37169ef094e4f2372734d9f2acef7787275abd
Matteo-K <kervadecmatteo@gmail.com>
2024-11-27 15:32:15 +0100

    offre

--- html/components/cardALaUne.php
+++ html/components/cardALaUne.php
@@ -7,9 +7,23 @@
       <h3><?php echo $nomOffre ?></h3>
       <div>
         <div id="detailsCardOffer">
+          <div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <div class="star"></div>
+            <span><?php echo number_format($noteAvg) ?>/5</span>
+            <span>(<?php echo $nbNote ?>avis)</span>
+            <?php if ($categorie == "Restaurant") { ?>
+              <span> ⋅ <?php echo $gammeDePrix ?></span>
+            <?php } ?>
+          </div>
           <p><?php echo $resume ?></p>
         </div>
         <div id="localisationCard">
+          <adresse><?php echo $ville ?>, <?php echo $codePostal ?></adresse>
+          <h4>Catégories&nbsp;:&nbsp;</h4>
           <div id="tagsCard">
             <?php foreach ($tags as $key => $tag) { ?>
               <span class="tagIndex"><?php echo $tag ?></span>
