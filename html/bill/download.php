c537464dc8aff7aabcbf74c1ef95898060349c70
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-27 13:40:41 +0100

    test

--- html/bill/download.php
+++ html/bill/download.php
@@ -7,25 +7,4 @@ if ($_SERVER['REQUEST_METHOD'] !== "POST") {
 }
 
 
-use Dompdf\Dompdf;
-
-// Initialisation de Dompdf
-$dompdf = new Dompdf();
-
-// HTML à convertir
-ob_start();
-include 'bill.php'; // Inclut le fichier HTML
-$html = ob_get_clean();
-
-// Charger le contenu HTML
-$dompdf->loadHtml($html);
-
-// Configurer le format et l'orientation de la page
-$dompdf->setPaper('A4', 'portrait'); // Options : A4, Letter, etc.
-
-// Rendre le HTML en PDF
-$dompdf->render();
-
-// Télécharger le fichier PDF
-$dompdf->stream("document.pdf", ["Attachment" => true]); // "Attachment" => true pour forcer le téléchargement
-?>
+print_r($_POST);
