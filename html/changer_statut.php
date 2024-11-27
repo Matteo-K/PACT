6aa83533011718b5c15000b642365992eab8ce8c
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-27 23:21:18 +0100

    ajustememnterreur

--- html/changer_statut.php
+++ html/changer_statut.php
@@ -5,7 +5,6 @@ require_once "config.php";
 // Récupérer les données du formulaire
 $offreId = $_POST['offre_id'];
 $nouveauStatut = $_POST['nouveau_statut'];
-$ouvert= $_POST['ouvert'];
 
 // Mettre à jour le statut de l'offre
 $stmt = $conn->prepare("UPDATE pact._offre SET statut = :statut WHERE idoffre = :id");
@@ -54,6 +53,6 @@ if ($nouveauStatut=='actif') {
 }
 
 // Rediriger vers la page précédente ou une autre page
-header("Location: detailsOffer.php?idoffre=$offreId&ouvert=$ouvert");
+header("Location: detailsOffer.php");
 exit;
 ?>
