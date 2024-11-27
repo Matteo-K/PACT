8d77d0a4f82ee809fef665ab095a8d647bf8d032
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-27 13:27:32 +0100

    test download

--- html/components/header.php
+++ html/components/header.php
@@ -126,6 +126,9 @@
                                                             <button class="modifierBut" type="submit">Visualiser</button>
                                                         </form>
                                                         <form action="" method="post">
+                                                        <input type="hidden" name="idOffre" value="<?php echo $value['idoffre']; ?>">
+                                                        <input type="hidden" name="mois" value="<?php echo $moisEnFrancais; ?>">
+                                                        <input type="hidden" name="date" value="<?php echo $value2; ?>">
                                                             <button class="modifierBut" type="submit">Télécharger</button>
                                                         </form>
                                                     </div>
