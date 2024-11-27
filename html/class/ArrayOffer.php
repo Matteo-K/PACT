f5dd4320f2bff9c378b77b3adf3c0d1bfaa22706
Matteo-K <kervadecmatteo@gmail.com>
2024-11-27 11:16:48 +0100

    option remove display

--- html/class/ArrayOffer.php
+++ html/class/ArrayOffer.php
@@ -92,7 +92,6 @@ class ArrayOffer {
         $stmt = $conn->prepare("SELECT * FROM pact._option_offre WHERE idoffre = ? AND idoption = (SELECT MAX(idoption) FROM pact._option_offre WHERE idoffre = ?)");
         $stmt->execute([$offre['idoffre'], $offre['idoffre']]);
         while ($resOption = $stmt->fetch(PDO::FETCH_ASSOC)) {
-          print_r($offre);
           $options[] = $resOption["nomoption"];
         }
         
