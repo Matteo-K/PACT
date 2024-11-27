6a308e85c78a298fa755f76e2cd6c07ec0c3675b
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-27 16:38:31 +0100

    test

--- html/components/header.php
+++ html/components/header.php
@@ -174,16 +174,6 @@
             const profilePic = document.getElementById("profilePic");
             const profileMenu = document.getElementById("profileMenu");
             const backButton = document.getElementById("backButton");
-            const factu = document.getElementsByClassName("liFact")[0];
-            const DivFactue = document.getElementsByClassName("factue")[0];
-
-            function toggleFacture() {
-                if (DivFactue.style.display === "none") {
-                    DivFactue.style.display = "flex"
-                } else {
-                    DivFactue.style.display = "none"; // Cache la div
-                }
-            }
             
             factu.addEventListener("click", toggleFacture);
             // Fonction pour afficher/cacher le menu
