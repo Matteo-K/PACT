ae1fb4bc1ebb4d8582adffe8f932275466d9feb9
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-27 23:59:01 +0100

    alors

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -225,7 +225,7 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             class="modifierBut <?php echo $offre[0]['statut'] === 'actif' ? 'disabled' : ''; ?>" 
                             type="submit"
                             <?php if ($offre[0]['statut'] === 'actif') { ?>
-                                onclick="return false;" <!-- Empêche le clic -->
+                                onclick="return false;"
                             <?php } ?>
                         >
                             <?php echo "Modifier offre"; ?>
