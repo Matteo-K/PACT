f6d56462adca5908830088262bd0e6a7ec50d370
ejain6 <ewen.jain@gmail.com>
2024-11-28 15:55:11 +0100

    JE COMMENCE LES AVIS PRO

---
+++ html/components/avis/avisPro.php
@@ -0,0 +1,26 @@
+
+
+<section>
+
+    <h2 class="triangle">
+    
+        <img src="" alt="">
+        Avis les plus récents
+    </h2>
+
+    <div>
+
+    </div>
+
+
+</section>
+
+
+<section>
+
+    <div>
+        
+    </div>
+
+
+</section>
