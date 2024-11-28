d234085e244b1ad1264413efb5f4b0c4751d303e
Arkaaade <antoine.guillerm@etudiant.univ-rennes.fr>
2024-11-28 10:47:29 +0100

    changePassword

---
+++ html/changePassword.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/logo.png" type="image/x-icon">
+    <title>Changer de mot de passe</title>
+</head>
+<body id ="body_connexion" class="connexion-compte">
+    <aside id="asideRetour">
+        <button id="retour">
+            <img src="img/logo.png" alt="Logo" title="Retour page précédente"/>
+            Retour
+        </button>
+    </aside>
+
+    <main id="mainConnexion">
+        <h1 id="connexionTitre">Changer de mot de passe</h1>
+
+        <?php if (!empty($error)): ?>
+            <div id="messageErreur" class="messageErreur"><?php echo htmlspecialchars($error); ?></div>
+        <?php endif; ?>
+
+        <form id = "formConnexion" action="login.php" method="post" enctype="multipart/form-data">
+            <div class="ligne1">
+                <!-- Saisi du login -->
+                <input type="password" placeholder="ancient mot de passe" id="login" name="login" required>
+            </div>
+    
+    
+            <div class="ligne2">
+                <!-- Saisi du mot de passe -->
+                <input type="password" placeholder="nouveau mot de passe" id="motdepasseConnexion" name="motdepasseConnexion" required>
+            </div>
+
+
+            <div class="ligne2">
+                <!-- Saisi du mot de passe -->
+                <input type="password" placeholder="Confirmer le nouveau mot de passe" id="motdepasseConnexion" name="motdepasseConnexion" required>
+            </div>
+    
+            <button onclick = "validationFormConnexion()" id="boutonConnexion">Changer le mot de passe</button>
+        </form>
+    </main>
+</body>
+<script src = "js/validationFormInscription.js"></script>
+</html>
