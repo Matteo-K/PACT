cd34b7ce68b20d4bc0287fad14315ac29fdf6925
Matteo-K <kervadecmatteo@gmail.com>
2024-11-28 11:13:10 +0100

    search

--- html/search.php
+++ html/search.php
@@ -106,11 +106,8 @@ $arrayOffer = [];
         </section>
     </main>
     <?php require_once "components/footer.php"; ?>
-    <?php if (empty($search)) { ?>
-        <div id="offers-data" data-offers='<?php echo htmlspecialchars(json_encode($offres->getArray($offres->filtre($idUser, $typeUser)))); ?>'></div>
-    <?php } else { ?>
-        <div id="offers-data" data-offers='<?php echo htmlspecialchars(json_encode($offres->getArray($offres->recherche($idUser, $typeUser, $search)))); ?>'></div>
-    <?php } ?>
+    <!-- Data -->
+    <div id="offers-data" data-offers='<?php echo htmlspecialchars(json_encode($offres->getArray($offres->recherche($idUser, $typeUser, $search)))); ?>'></div>
     <div id="user-data" data-user='<?php echo $typeUser ?>'></div>
     <script src="js/sortAndFilter.js"></script>
     <script>
