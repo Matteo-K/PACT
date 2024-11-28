3b7e6367b2c3926f3b05a5d2287f1ca1249c9e67
Matteo-K <kervadecmatteo@gmail.com>
2024-11-28 12:15:12 +0100

    tets

--- html/search.php
+++ html/search.php
@@ -1,6 +1,7 @@
 <?php 
 require_once "config.php";
 
+$search = isset($_GET["search"]) ? $_GET["search"] : "";
 $page = isset($_GET["page"]) ? $_GET["page"] :  1;
 $nbElement = 15;
 $countOffer = 0;
@@ -108,6 +109,7 @@ $arrayOffer = [];
     <?php require_once "components/footer.php"; ?>
     <?php print_r($_GET); ?>
     <?php echo $search ?>
+    <?php print_r($offres->getArray($offres->recherche($idUser, $typeUser, $search))); ?>
     <!-- Data -->
     <div id="offers-data" data-offers='<?php echo htmlspecialchars(json_encode($offres->getArray($offres->recherche($idUser, $typeUser, $search)))); ?>'></div>
     <div id="user-data" data-user='<?php echo $typeUser ?>'></div>
