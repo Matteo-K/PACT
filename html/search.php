7392ad9be82c476bd1cee0ba2573de472dce6ed0
Matteo-K <kervadecmatteo@gmail.com>
2024-11-28 11:02:59 +0100

    réparation temporaire

--- html/search.php
+++ html/search.php
@@ -107,7 +107,11 @@ $arrayOffer = [];
         </section>
     </main>
     <?php require_once "components/footer.php"; ?>
-    <div id="offers-data" data-offers='<?php echo htmlspecialchars(json_encode($offres->getArray($offres->recherche($idUser, $typeUser, $search)))); ?>'></div>
+    <?php if (empty($search)) { ?>
+        <div id="offers-data" data-offers='<?php echo htmlspecialchars(json_encode($offres->getArray($offres->filtre($idUser, $typeUser)))); ?>'></div>
+    <?php } else { ?>
+        <div id="offers-data" data-offers='<?php echo htmlspecialchars(json_encode($offres->getArray($offres->recherche($idUser, $typeUser, $search)))); ?>'></div>
+    <?php } ?>
     <div id="user-data" data-user='<?php echo $typeUser ?>'></div>
     <script src="js/sortAndFilter.js"></script>
     <script>
