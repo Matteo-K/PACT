062ae4a458c0b97a8b39bb60dc79870f0e9fb67b
ejain6 <ewen.jain@gmail.com>
2024-11-28 15:19:50 +0100

    testttttt

--- html/components/offer/detailsOffer.php
+++ html/components/offer/detailsOffer.php
@@ -286,8 +286,10 @@ while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
             nouvelInputFile.name = "ajoutPhoto[]";
             nouvelInputFile.accept = "image/PNG, image/JPG, image/JPEG, image/WEBP, image/GIF";
 
+            inputFile.removeEventListener("change", afficheImage);
             inputFile.replaceWith(nouvelInputFile); // Remplacer l'ancien input
             inputFile = nouvelInputFile; // Mettre à jour la référence
+            
             inputFile.addEventListener("change", afficheImage);
             console.log("Nouvel input créé et événement attaché.");
         }
