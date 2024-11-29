175fd66e33d86694d73dc6fcdf80ac356bdb75ea
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-11-29 10:14:49 +0100

    hop

--- html/components/offer/details/detailsActivity.php
+++ html/components/offer/details/detailsActivity.php
@@ -25,59 +25,61 @@ if ($categorie["_activite"]) {
 <section id="activity">
 
 
-    
-        <div class="divAge">
-            <label class="labAgeAct" name="labAgeAct"> Age: </label>
-            <input type="number" id="numberAct" name="ageAct" min="0" placeholder="0" />
-            <label class="labAnsAct" name="labAgeAct"> Ans </label>
+
+    <div class="divAge">
+        <label class="labAgeAct" name="labAgeAct"> Age: </label>
+        <input type="number" id="numberAct" name="ageAct" min="0" placeholder="0" />
+        <label class="labAnsAct" name="labAgeAct"> Ans </label>
+    </div>
+    <div class="presta">
+        <label>Prestation(s)</label>
+
+        <textarea name="textPrestationsAct" id="textePrestation" placeholder="Entrer une prestation "></textarea>
+
+        <input type="button" id="buttonAjoutPresta" name="BtnAjoutPresta" value="Ajouter des presations">
+
+
+    </div>
+
+
+
+    <div class="divP">
+        <label>Prix minimum</label>
+
+
+        <input type="number" id="PrixMinAct" name="PrixMinAct" min="0" placeholder="0">
+        <label>€</label>
+
+    </div>
+    <!-- Partie accéssibilité à modifiier avec la bdd a jour -->
+    <div class="access">
+        <label id="labAccess">Accessibilité</label> <!-- Label Accessibilité -->
+
+
+        <div class="acces1">
+            <input type="radio" id="Acces" name="Accessibilite" value="Acces" checked>
+            <!-- Bouton radio pour le choix de l'accesibilite PMR il est lier avec le 2eme et est selectionner par defaut -->
+            <label for="Acces"> Accès personne handicapées</label>
+            <!-- Label associé au bouton radio -->
         </div>
-        <div class="presta">
-            <label>Prestation(s)</label>
-
-            <textarea name="textPrestationsAct" id="textePrestation" placeholder="Entrer une prestation "></textarea>
-
-            <input type="button" id="buttonAjoutPresta" name="BtnAjoutPresta" value="Ajouter des presations">
-
-
+        <div class="access1">
+            <input type="radio" id="pasAcces" name="Accessibilite" value="pasAcces">
+            <!-- 2eme bouton radio liés au 1er via l'id -->
+            <label for="pasAcces"> Pas d’accès personne handicapées </label>
+            <!-- Label du 2eme bouton radio -->
         </div>
-
-    
-
-        <div class="divP">
-            <label>Prix minimum</label>
+    </div>
 
 
-            <input type="number" id="PrixMinAct" name="PrixMinAct" min="0" placeholder="0">
-            <label>€</label>
-
-        </div>
-        <!-- Partie accéssibilité à modifiier avec la bdd a jour -->
-        <div class="access">
-            <label id="labAccess">Accessibilité</label> <!-- Label Accessibilité -->
 
 
-            <div class="acces1">
-                <input type="radio" id="Acces" name="Accessibilite" value="Acces" checked>
-                <!-- Bouton radio pour le choix de l'accesibilite PMR il est lier avec le 2eme et est selectionner par defaut -->
-                <label for="Acces"> Accès personne handicapées</label>
-                <!-- Label associé au bouton radio -->
-            </div>
-            <div class="access1">
-                <input type="radio" id="pasAcces" name="Accessibilite" value="pasAcces">
-                <!-- 2eme bouton radio liés au 1er via l'id -->
-                <label for="pasAcces"> Pas d’accès personne handicapées </label>
-                <!-- Label du 2eme bouton radio -->
-            </div>
-        </div>
-
-
-    
-    
-        <div>
-            <label class="labDuréeAct" name="labDuréeAct"> Durée: </label>
+    <div class="divD">
+        <label class="labDuréeAct" name="labDuréeAct"> Durée: </label>
+        <div class="divD1">
             <input type="number" id="numberAct" name="duréeAct" placeholder="0" />
             <label class="labHAct" name="labHAct"> H </label>
-
         </div>
-    
+
+    </div>
+
 </section>
