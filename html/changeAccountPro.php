c159ba35dcc28b2e321fd34f3afcb887f6b59b61
Arkaaade <antoine.guillerm@etudiant.univ-rennes.fr>
2024-11-29 18:44:13 +0100

    récup des données ?

--- html/changeAccountPro.php
+++ html/changeAccountPro.php
@@ -16,7 +16,7 @@
 
     // Récupérer les informations de l'utilisateur depuis la base de données
     try {
-        $stmt = $conn->prepare("SELECT * FROM pact._pro WHERE idU = ?");
+        $stmt = $conn->prepare("SELECT * FROM pact._pro WHERE denomination = ?");
         $stmt->execute([$userId]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
