99b35b105b665970c2f6d8573310dd93b397205d
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-11-30 17:24:21 +0100

    enfin

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -330,12 +330,9 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <section id="myModal" class="modal">
               <section class="modal-content">
                 <span class="close">&times;</span>
-                <h2>Formulaire dans un popup</h2>
+                <h2>Gestion des option</h2>
                 <form id="popupForm" class="option">
-                  <label for="name">Nom :</label>
-                  <input type="text" id="name" name="name" placeholder="Entrez votre nom" required>
-                  <label for="email">Email :</label>
-                  <input type="email" id="email" name="email" placeholder="Entrez votre email" required>
+                  
                   <button type="submit">Envoyer</button>
                 </form>
               </section>
