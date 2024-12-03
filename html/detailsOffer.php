05cebd80de614f1dd5cd7d06cd89215dc8fc8377
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-12-03 11:51:11 +0100

    test

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -279,12 +279,16 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         $option = $conn->prepare("SELECT * FROM pact.option WHERE idoffre=? and (datefin>CURRENT_DATE OR datefin is null) and nomoption = 'EnRelief'");
                         $option->execute([$idOffre]);
                         $optionRelief = $option->fetchAll(PDO::FETCH_ASSOC);
-                        $mesOtion;
+                        $mesOtion = [];
                         if ($optionRelief) {
-                            $mesOtion[] = $optionRelief;
+                            foreach ($optionRelief as $key => $value) {
+                                $mesOtion[] = $value;
+                            }
                         }
                         if ($optionUne) {
-                            $mesOtion[] = $optionUne;
+                            foreach ($optionUne as $key => $value) {
+                                $mesOtion[] = $value;
+                            }
                         }
                         if ($mesOtion != []) {
                             ?>
