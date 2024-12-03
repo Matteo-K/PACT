4cbfd94ff550f67bf81ace53ac85bc1d3f2c0c7f
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-12-03 11:42:10 +0100

    ajout offre pdt en ligne directement en ligne

--- html/addOption.php
+++ html/addOption.php
@@ -2,20 +2,37 @@
 
 require_once 'config.php';
 
+$offreId = $_POST['idOffre'];
+
 if ($_POST['type'] == 'ajout') {
     if ($_POST['nomOption']=='ALaUne') {
-        $duree = $_POST['nbWeek']*20;
+        $prix = $_POST['nbWeek']*20;
     } else {
-        $duree = $_POST['nbWeek']*10;
+        $prix = $_POST['nbWeek']*10;
     }
-    $stmt = $conn->prepare("INSERT INTO pact.option (idOffre,dateLancement,dateFin,duree_total,prix_total,nomOption) VALUES (?,NULL,NULL,?,?,?)");
-    $stmt->execute([$_POST['idOffre'], $_POST['nbWeek'], $duree , $_POST['nomOption']]);
+
+    $stmt = $conn->prepare("SELECT statut FROM pact.offres WHERE idoffre=?");
+    $stmt->execute([$offreId]);
+    $EnLigne = $stmt->fetchAll();
+    if ($EnLigne[0]['statut'] == 'actif') {
+        $duree = $_POST['nbWeek'] * 7;
+        $date = new DateTime();
+        $date->modify("+$duree days"); // Ajout de la durée
+        $formattedDate = $date->format('Y-m-d'); // Conversion de l'objet DateTime en format SQL compatible
+                
+        $idoption = $offreId;
+                        
+        $stmt = $conn->prepare("INSERT INTO pact.option (idOffre,dateLancement,dateFin,duree_total,prix_total,nomOption) VALUES (?,CURRENT_DATE,?,?,?,?)");
+        $stmt->execute([$_POST['idOffre'], $formattedDate , $_POST['nbWeek'], $prix , $_POST['nomOption']]);
+    }else {
+        $stmt = $conn->prepare("INSERT INTO pact.option (idOffre,dateLancement,dateFin,duree_total,prix_total,nomOption) VALUES (?,NULL,NULL,?,?,?)");
+        $stmt->execute([$_POST['idOffre'], $_POST['nbWeek'], $prix , $_POST['nomOption']]);
+    }
 }
 
 $stmt = $conn->prepare("SELECT * FROM pact.option");
 $stmt->execute();
 
-$offreId = $_POST['idOffre'] ;
 
 echo <<<HTML
 <form id="redirectForm" method="POST" action="detailsOffer.php">
