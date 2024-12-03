cde9576924039d2e6b1cdf08ea51384a71f97d98
Matteo-K <kervadecmatteo@gmail.com>
2024-12-03 22:15:27 +0100

    affichage class

---
+++ html/class/Main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Class Main</title>
+</head>
+<body>
+  <?php require_once __DIR__."../components/header.php" ?>
+  <?php $offres = new ArrayOffer(); ?>
+  <main>
+    <?php 
+        $getOffres = $offres->getArray();
+        foreach ($getOffres as $key => $value) {
+            print_r($value); ?> <br> <?php
+        }
+    ?>
+  </main>
+</body>
+</html>
