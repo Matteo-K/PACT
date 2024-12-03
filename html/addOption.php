3d93a17e6a38fe46444b74a5b8750b67d322cae9
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-12-03 10:14:36 +0100

    affiche tout les option

--- html/addOption.php
+++ html/addOption.php
@@ -9,4 +9,8 @@ if ($_POST['type'] == 'ajout') {
     $stmt = $conn->prepare("INSERT INTO pact.option (idOffre,dateLancement,dateFin,duree_total,prix_total,nomOption) VALUES (?,NULL,NULL,?,?,?)");
     $stmt->execute([$_POST['idOffre'], $_POST['nbWeekALaUne'], $duree , $value['nomOption']]);
 }
+
+$stmt = $conn->prepare("SELECT * FROM pact.option");
+$stmt->execute();
+print_r($stmt->fetchAll());
 ?>
