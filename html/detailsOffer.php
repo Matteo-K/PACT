7941f9c498045d9ec97323dc84727f12e040db40
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2024-12-03 09:02:34 +0100

    hip hop

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -364,7 +364,7 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                 <form class="donneeForm" action="" method="post">
                                     <input type="hidden" name="nomOption" value="ALaUne">
                                     <label class="taille" for="nbWeekALaUne">Nombre de semaine en Relief</label>
-                                    <input class="taille2" type="number" name="nbWeekALaUne" id="nbWeekALaUne" min="1" max="4">
+                                    <input class="taille2" type="number" name="nbWeekALaUne" id="nbWeekALaUne" min="1" max="4" value="1">
                                 </form>
                                 <?php
                                 if (!$optionUne) {
