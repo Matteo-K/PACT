c9ca7f2b8ca20fd9d493d2e4913000745cdac9ee
Matteo-K <kervadecmatteo@gmail.com>
2024-12-03 22:53:52 +0100

    toujours pbm avis null

--- html/class/ArrayOffer.php
+++ html/class/ArrayOffer.php
@@ -220,8 +220,6 @@ class ArrayOffer {
         $stmt = $conn->prepare("SELECT a.* from pact.avis a where idoffre = ? order by datepublie desc limit 2");
         $stmt->execute([$offre['idoffre']]);
         while ($resAvis = $stmt->fetch(PDO::FETCH_ASSOC)) {
-          echo "pass";
-          print_r($resAvis);
           $avis[] = [
             "titre" => $resAvis["titre"], 
             "contenue" => $resAvis["content"],
@@ -229,6 +227,8 @@ class ArrayOffer {
             "note" => $resAvis["note"]
           ];
         }
+
+        print_r($avis);
 
         $this->arrayOffer[$offre['idoffre']]->setData($offre['idoffre'], 
           $offre['idu'], $offre['nom'],
