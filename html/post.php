adf32f510a0b6b5c7c191eda30399b9551f282b5
Matteo-K <kervadecmatteo@gmail.com>
2024-12-05 23:15:00 +0100

    test date français

--- html/post.php
+++ html/post.php
@@ -1,13 +1,22 @@
 <?php
 
 print_r($_POST);
-$date = $_POST["dates"];
-$dates = $date[1]["trip-start"];
+// Définir la locale en français
+setlocale(LC_TIME, 'fr_FR.UTF-8');
 
-$formattedDate = date("l Y-m-d", strtotime($dates));
-$jour = explode(" ",$formattedDate);
+// Supposons que `$dates` contient la date en format "Y-m-d"
+$date = $_POST["dates"];
+$dates = $date[1]["trip-start"]; // Par exemple : "2024-12-05"
+
+// Formater la date en français
+$timestamp = strtotime($dates);
+$formattedDate = strftime("%A %Y-%m-%d", $timestamp); // Format : "jeudi 2024-12-05"
+
+// Découper en jour de la semaine et date
+$jour = explode(" ", $formattedDate);
 
 print_r($jour);
+
 
 
 ?>
