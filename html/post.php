3d4f5155c085dd2c82665fe366ca48cebcb8102c
Matteo-K <kervadecmatteo@gmail.com>
2024-12-05 23:02:38 +0100

    correction

--- html/post.php
+++ html/post.php
@@ -4,7 +4,7 @@ print_r($_POST);
 $date = $_POST["dates"];
 $dates = $date[1]["trip-start"];
 
-$formattedDate = date("l Y-m-d", strtotime($date));
+$formattedDate = date("l Y-m-d", strtotime($dates));
 $jour = explode(" ",$formattedDate);
 
 print_r($jour);
