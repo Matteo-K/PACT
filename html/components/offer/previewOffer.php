0cc7f262b2fe000f6d1443608bba143a65948b19
Arkaaade <antoine.guillerm@etudiant.univ-rennes.fr>
2024-12-05 16:14:20 +0100

    data preview

--- html/components/offer/previewOffer.php
+++ html/components/offer/previewOffer.php
@@ -109,7 +109,7 @@ $photos = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 ?>
                     <div>
                         <img src="./img/icone/lieu.png">
-                        <a href="https://www.google.com/maps?q=<?php echo urlencode($data[$idOffre]["numeroRue"] . " " . $data[$idOffre]["rue"] . ", " . $data[$idOffre]["codePostal"] . " " . $data[$idOffre]["ville"]); ?>" target="_blank" id="lieu"><?php echo htmlspecialchars($result[0]["numerorue"] . " " . $result[0]["rue"] . ", " . $result[0]["codepostal"] . " " . $result[0]["ville"]); ?></a>
+                        <a href="https://www.google.com/maps?q=<?php echo urlencode($data[$idOffre]["numeroRue"] . " " . $data[$idOffre]["rue"] . ", " . $data[$idOffre]["codePostal"] . " " . $data[$idOffre]["ville"]); ?>" target="_blank" id="lieu"><?php echo htmlspecialchars($data[$idOffre]["numeroRue"] . " " . $data[$idOffre]["rue"] . ", " . $data[$idOffre]["codePostal"] . " " . $data[$idOffre]["ville"]); ?></a>
                     </div>
                     <div>
                         <img src="./img/icone/tel.png">
