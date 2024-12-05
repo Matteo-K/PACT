df40fee243db80647fd93bf27bd2deb9ff02cb6b
Matteo-K <kervadecmatteo@gmail.com>
2024-12-05 18:25:39 +0100

    date

--- html/components/offer/hourlyOffer.php
+++ html/components/offer/hourlyOffer.php
@@ -153,11 +153,15 @@
                     console.error("Erreur de parsing JSON :", error);
                 }
 
-                console.log(arrayOffer[0].horaire);
+                const listHoraire = arrayOffer[0].horaire.map(item => JSON.parse(item));
 
-                arrayOffer.forEach(element => {
-                    
-                });
+                if (listHoraire.length != 0) {
+                    listHoraire.forEach(element => {
+                        addDateRep(element);
+                    });
+                } else {
+                    addDateRep();
+                }
             });
             /* Interraction horaire */
             let counterRep = 1;
@@ -236,23 +240,21 @@
                     });
                 } else {
 
-                    inputHoraire1.setAttribute("value", "19:00");
-                    inputHoraire2.setAttribute("value", "19:00");
+                    inputHoraire1.setAttribute("value", data.heureouverture);
+                    inputHoraire2.setAttribute("value", data.heurefermeture);
                     
                     flatpickr("#"+date.id, {
                         altInput: true,
                         altFormat: "l j F Y",
                         dateFormat: "Y-m-d",
                         locale: "fr",
-                        defaultDate: "2024-12-05",
+                        defaultDate: data.daterepresentation,
                         minDate: current_date
                     });
                 }
 
 
             }
-
-            addDateRep();
 
             /**
              * @brief Retire le bloc Représentation
