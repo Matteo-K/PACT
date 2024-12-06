766691b7229f33a6d9ce3d0bd337487b0834056d
Matteo-K <kervadecmatteo@gmail.com>
2024-12-06 14:28:26 +0100

    test

--- html/js/script.js
+++ html/js/script.js
@@ -121,8 +121,41 @@ try {
     
   } catch (error) { }
 
-/* gestion des payments */
 
+try {
+  const btnsAddHourly = document.querySelectorAll(".btnAddOffer");
+        const btnsRmHourly = document.querySelectorAll(".btnRmOffer");
+
+        /**
+         * @brief Ajoute une horaire à la journée
+         */
+        btnsAddHourly.forEach((button) => {
+            button.addEventListener("click", () => {
+                let nextSpan = button.nextElementSibling;
+                let nextBtn = nextSpan.nextElementSibling;
+                nextSpan.classList.remove("hourlyHide");
+                nextBtn.classList.remove("hourlyHide");
+                button.classList.add("hourlyHide");
+            });
+        });
+
+        /**
+         * @brief Retire une horaire à la journée
+         */
+        btnsRmHourly.forEach((button) => {
+            button.addEventListener("click", () => {
+                let span = button.previousElementSibling;
+                span.querySelectorAll("input").forEach((input) => {
+                    input.value = "";
+                });
+                span.classList.add("hourlyHide");
+                button.classList.add("hourlyHide");
+                span.previousElementSibling.classList.remove("hourlyHide");
+            });
+        });
+} catch (error) {}
+
+/* gestion des payments */
 try {
   const radBtnCB = document.querySelector("#carte_bancaire");
   const radBtnVB = document.querySelector("#virement_bancaire");
