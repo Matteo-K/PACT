0cf600ea10eeb222a1a28be8c08bc06c42797d70
Matteo-K <kervadecmatteo@gmail.com>
2024-12-06 13:18:10 +0100

    arrayOffer

--- html/class/ArrayOffer.php
+++ html/class/ArrayOffer.php
@@ -262,6 +262,8 @@ class ArrayOffer {
   public function recherche($idUser_, $typeUser_, $recherche) {
     $array = $this->filtre($idUser_, $typeUser_);
 
+    print_r($array);
+
     if (empty($recherche)) {
         return $array;
     }
