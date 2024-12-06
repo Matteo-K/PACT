fbe3466a1450209995e5ce00112b06fc567a94c5
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2024-12-06 10:07:38 +0100

    -rds

--- html/components/offer/details/detailsVisit.php
+++ html/components/offer/details/detailsVisit.php
@@ -224,18 +224,18 @@ if ($categorie["_visite"]) {
 
                 // Ajouter un bouton de suppression avec une icône
 
-                 const removeBtn = document.createElement('button');
-                 removeBtn.innerHTML = '<span style="font-size: 1.2em; color: #d32f2f;">&times;</span>'; 
-                 removeBtn.className = 'remove-btn';
+                //  const removeBtn = document.createElement('button');
+                //  removeBtn.innerHTML = '<span style="font-size: 1.2em; color: #d32f2f;">&times;</span>'; 
+                //  removeBtn.className = 'remove-btn';
 
                 // Pour supprimer le tag 
                 removeBtn.addEventListener('click', function () {
                     sectionLangue.removeChild(langDiv);
                 });
 
-                // Ajouter le texte et le bouton au conteneur de langue
+                
                 langDiv.appendChild(langText);
-                langDiv.appendChild(removeBtn);
+              //  langDiv.appendChild(removeBtn);
 
                 // Appliquer un style visuel au conteneur
 
