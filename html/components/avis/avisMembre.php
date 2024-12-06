78009a2184f27ca75bce923f1b83c4958077ff71
BZHKylian <kylian.houedec.56@gmail.com>
2024-12-06 10:26:27 +0100

    test

--- html/components/avis/avisMembre.php
+++ html/components/avis/avisMembre.php
@@ -117,7 +117,7 @@ foreach ($avis as $a) {
 }
 ?>
 
-
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script>
     var swiper3 = new Swiper(".mySwiperAvis", {
         loop: true,
