2d67d0325c63b63ec88328009365adc7775dbb8e
Arkaaade <antoine.guillerm@etudiant.univ-rennes.fr>
2024-12-06 10:02:05 +0100

    .

--- html/changeAccountPro.php
+++ html/changeAccountPro.php
@@ -142,6 +142,12 @@
 
         <form id = "formPro" action="changeAccountPro.php" method="post" enctype="multipart/form-data">
 
+
+            <div id="divPFPactuelle">
+                <img src="uploads/<?= htmlspecialchars($photoProfil) ?>" alt="Photo de Profil" id="current-profile-pic">
+                <input type="file" id="profile-pic" name="profile-pic" accept="image/*" required>
+            </div>
+
             <div id="divPFP">
                 <img src="uploads/<?= htmlspecialchars($photoProfil) ?>" alt="Photo de Profil" id="current-profile-pic">
                 <input type="file" id="profile-pic" name="profile-pic" accept="image/*" required>
