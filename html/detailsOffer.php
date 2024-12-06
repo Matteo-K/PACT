b1d45d69aa7b5ed685d3f266c3cf7d5aaa15d91a
BZHKylian <kylian.houedec.56@gmail.com>
2024-12-06 10:37:02 +0100

    test

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -513,7 +513,7 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
             ?>
         
-        <h2 id=" titleOffer"><?php echo htmlspecialchars($result[0]["nom"]); ?></h2>
+        <h2 id="titleOffer"><?php echo htmlspecialchars($result[0]["nom"]); ?></h2>
                     <h3 id="typeOffer"><?php echo $typeOffer ?> à <?php echo $result[0]['ville'] ?></h3>
                     <?php
                     if (($typeUser == "pro_public" || $typeUser == "pro_prive")) {
