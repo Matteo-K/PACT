c090adad2a03c83c821bc3360099b1672883b1bf
Matteo-K <kervadecmatteo@gmail.com>
2024-12-06 14:29:45 +0100

    script

--- html/components/offer/hourlyOffer.php
+++ html/components/offer/hourlyOffer.php
@@ -90,6 +90,38 @@ $is_show;
             </span>
         </div>
     </div>
+    <script>
+        const btnsAddHourly = document.querySelectorAll(".btnAddOffer");
+        const btnsRmHourly = document.querySelectorAll(".btnRmOffer");
+
+        /**
+         * @brief Ajoute une horaire à la journée
+         */
+        btnsAddHourly.forEach((button) => {
+            button.addEventListener("click", () => {
+                let nextSpan = button.nextElementSibling;
+                let nextBtn = nextSpan.nextElementSibling;
+                nextSpan.classList.remove("hourlyHide");
+                nextBtn.classList.remove("hourlyHide");
+                button.classList.add("hourlyHide");
+            });
+        });
+
+        /**
+         * @brief Retire une horaire à la journée
+         */
+        btnsRmHourly.forEach((button) => {
+            button.addEventListener("click", () => {
+                let span = button.previousElementSibling;
+                span.querySelectorAll("input").forEach((input) => {
+                    input.value = "";
+                });
+                span.classList.add("hourlyHide");
+                button.classList.add("hourlyHide");
+                span.previousElementSibling.classList.remove("hourlyHide");
+            });
+        });
+    </script>
     <?php
         }
     } else if ($is_show == 1) {
