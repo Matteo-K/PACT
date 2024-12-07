9b74dad3e3d5f13dfac6da182bb780c5c7794deb
Matteo-K <kervadecmatteo@gmail.com>
2024-12-07 01:34:48 +0100

    search dans index

--- html/index.php
+++ html/index.php
@@ -13,33 +13,34 @@ require_once "config.php";
 <body>
   <script src="js/setColor.js"></script>
   <?php require_once "components/header.php"; ?>
-  <main id="index">
-    <div id="ALaUne">
-      <?php if ($typeUser != "pro_public" && $typeUser != "pro_prive") { ?>
-        <h2>À la une</h2>
-      <?php } ?>
-      <div>
-        <?php 
+  <main>
+    <div id="index">
+      <div id="ALaUne">
+        <?php if ($typeUser != "pro_public" && $typeUser != "pro_prive") { ?>
+          <h2>À la une</h2>
+          <?php } ?>
+          <div>
+            <?php 
           $elementStart = 0;
           $nbElement = 20;
           $offres = new ArrayOffer();
           $offres->displayCardALaUne($offres->filtre($idUser, $typeUser), $typeUser, $elementStart, $nbElement);
-        ?>
+          ?>
       </div>
     </div>
     <div id="voirPlus">
       <?php if ($typeUser == "pro_public" || $typeUser == "pro_prive") { ?>
         <a href="manageOffer.php" class="modifierBut">Créer une offre</a>  
-      <?php } ?>
+        <?php } ?>
         <a href="search.php" class="modifierBut">Voir plus</a>
-    </div>
-    <?php if ($typeUser == "membre") {
-      $stmt = $conn->prepare("SELECT idoffre FROM pact._consulter where idu = ? and dateconsultation = CURRENT_DATE;");
-      $stmt->execute([$_SESSION['idUser']]);
-      $idOffres = [];
-      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      </div>
+      <?php if ($typeUser == "membre") {
+        $stmt = $conn->prepare("SELECT idoffre FROM pact._consulter where idu = ? and dateconsultation = CURRENT_DATE;");
+        $stmt->execute([$_SESSION['idUser']]);
+        $idOffres = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
           $idOffres[] = $row['idoffre'];
-      } ?>
+        } ?>
       <div id="consultationRecente">
         <h2>Consulté récemment</h2>
         <div>
@@ -47,14 +48,15 @@ require_once "config.php";
             $nbElement = 20;
             $consultRecent = new ArrayOffer($idOffres);
             $consultRecent->displayConsulteRecemment($nbElement);
-          ?>
+            ?>
         <?php } else { ?>
-            <p>Aucune offre consultée récemment</p>
+          <p>Aucune offre consultée récemment</p>
           <?php } ?>
         </div>
       </div>
-    <?php } ?>
-    <!-- Partie de recherche -->
+      <?php } ?>
+    </div>
+      <!-- Partie de recherche -->
     <div class="search">
       <?php 
       require_once "components/asideTriFiltre.php";
