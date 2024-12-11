1cd5f88f1db143c70c4970cb1e8399812caaa289
Matteo-K <kervadecmatteo@gmail.com>
2024-12-11 23:36:11 +0100

    changement img position

--- html/index.php
+++ html/index.php
@@ -15,48 +15,46 @@ require_once "config.php";
   <?php require_once "components/header.php"; ?>
   <main>
     <div id="index">
-      <div id="imgPhare">
-        <div id="ALaUne">
-          <?php if ($typeUser == "pro_public" || $typeUser == "pro_prive") { ?>
-            <h2>Vos offres</h2>
-          <?php } ?>
-          <div>
-            <?php 
-            $elementStart = 0;
-            $nbElement = 20;
-            $offres = new ArrayOffer();
-            $offres->displayCardALaUne($offres->filtre($idUser, $typeUser), $typeUser, $elementStart, $nbElement);
-            ?>
-          </div>
-        </div>
-        <div id="voirPlus">
-          <?php if ($typeUser == "pro_public" || $typeUser == "pro_prive") { ?>
-            <a href="manageOffer.php" class="modifierBut">Créer une offre</a>  
-          <?php } ?>
-          <a href="search.php" class="modifierBut">Voir plus</a>
-        </div>
-        <?php if ($typeUser == "membre") {
-          $stmt = $conn->prepare("SELECT idoffre FROM pact._consulter where idu = ? and dateconsultation = CURRENT_DATE;");
-          $stmt->execute([$_SESSION['idUser']]);
-          $idOffres = [];
-          while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $idOffres[] = $row['idoffre'];
-        } ?>
-        <div id="consultationRecente">
-          <h2>Consulté récemment</h2>
-          <div>
-            <?php if (count($idOffres) > 0) {
-              $nbElement = 20;
-              $consultRecent = new ArrayOffer($idOffres);
-              $consultRecent->displayConsulteRecemment($nbElement);
-              ?>
-            <?php } else { ?>
-              <p>Aucune offre consultée récemment</p>
-            <?php } ?>
-          </div>
-        </div>
+      <div id="ALaUne">
+        <?php if ($typeUser == "pro_public" || $typeUser == "pro_prive") { ?>
+          <h2>Vos offres</h2>
         <?php } ?>
+        <div>
+          <?php 
+          $elementStart = 0;
+          $nbElement = 20;
+          $offres = new ArrayOffer();
+          $offres->displayCardALaUne($offres->filtre($idUser, $typeUser), $typeUser, $elementStart, $nbElement);
+          ?>
+        </div>
       </div>
+      <div id="voirPlus">
+        <?php if ($typeUser == "pro_public" || $typeUser == "pro_prive") { ?>
+          <a href="manageOffer.php" class="modifierBut">Créer une offre</a>  
+        <?php } ?>
+        <a href="search.php" class="modifierBut">Voir plus</a>
+      </div>
+      <?php if ($typeUser == "membre") {
+        $stmt = $conn->prepare("SELECT idoffre FROM pact._consulter where idu = ? and dateconsultation = CURRENT_DATE;");
+        $stmt->execute([$_SESSION['idUser']]);
+        $idOffres = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+          $idOffres[] = $row['idoffre'];
+      } ?>
+      <div id="consultationRecente">
+        <h2>Consulté récemment</h2>
+        <div>
+          <?php if (count($idOffres) > 0) {
+            $nbElement = 20;
+            $consultRecent = new ArrayOffer($idOffres);
+            $consultRecent->displayConsulteRecemment($nbElement);
+            ?>
+          <?php } else { ?>
+            <p>Aucune offre consultée récemment</p>
+          <?php } ?>
+        </div>
+      </div>
+      <?php } ?>
     </div>
       <!-- Partie de recherche -->
     <div id="searchIndex" class="search">
