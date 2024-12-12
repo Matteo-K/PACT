0dde008ff95bab60585df00a03f300532dfed0f2
Matteo-K <kervadecmatteo@gmail.com>
2024-12-12 08:27:15 +0100

    recherche tag

--- html/js/sortAndFilter.js
+++ html/js/sortAndFilter.js
@@ -520,7 +520,7 @@ function searchOffer(offers, search) {
     const adresse = `${numeroRue} ${rue} ${ville} ${pays} ${codePostal}`.toLowerCase();
 
     // Filtre les données
-    const containsTag = item.tags.some(tag => tag.toLowerCase() === search.toLowerCase());
+    const containsTag = item.tags.some(tag => tag.toLowerCase().includes(search.toLowerCase()));
     const matchesCategorie = categorie && categorie.toLowerCase().includes(search.toLowerCase());
     const matchesNomOffre = nomOffre && nomOffre.toLowerCase().includes(search.toLowerCase());
     const matchesAdresse = adresse && adresse.includes(search.toLowerCase());
