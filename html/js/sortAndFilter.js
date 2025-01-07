be75415610a1e526d83620e416a7af1891dd5a8a
Matteo-K <kervadecmatteo@gmail.com>
2025-01-07 11:09:49 +0100

    correction carte

--- html/js/sortAndFilter.js
+++ html/js/sortAndFilter.js
@@ -661,13 +661,13 @@ function createCard(offer) {
   front.style.backgroundImage = offer.images[0];
 
   let titre = document.createElement("h4");
-  titre.textContent = offer.titre;
+  titre.textContent = offer.nomOffre;
 
   let back = document.createElement("div");
   back.classList.add("flip-card-back");
 
-  front.appendChild(titre);
-  back.appendChild(titre);
+  front.appendChild(titre.cloneNode(true));
+  back.appendChild(titre.cloneNode(true));
 
   content.appendChild(front);
   content.appendChild(back);
