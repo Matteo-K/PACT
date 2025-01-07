54d19df3c39b863350f06ea52d35e37c6c070059
BZHKylian <kylian.houedec.56@gmail.com>
2025-01-07 13:39:35 +0100

    test

--- html/enregAvis.php
+++ html/enregAvis.php
@@ -129,6 +129,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
     <script>
         let form = document.createElement('form');
         form.action = "detailsOffer.php";
@@ -143,6 +151,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         document.body.appendChild(form);
         form.submit();
     </script>
+    </body>
+    </html>
     <?php
 }
 ?>
