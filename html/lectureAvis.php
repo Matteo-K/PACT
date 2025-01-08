79301604e97b96fa639d9fe353b503421ba9d506
ejain6 <ewen.jain@gmail.com>
2025-01-08 16:09:18 +0100

    test

--- html/lectureAvis.php
+++ html/lectureAvis.php
@@ -6,11 +6,12 @@ $id = json_decode($data[0]) ?? null;
 // Vérification si 'id' est bien défini
 if ($id != null) {
 
-    echo $id;
-
     $stmt = $conn->prepare("UPDATE pact._avis SET lu = true WHERE idc = ?");
+    $stmt = $conn->prepare("INSERT INTO pact._tag (nomtag) VALUES (?)");
     $stmt->execute($id);
     $result = $stmt->fetch();
+
+
 
 }
 else{
