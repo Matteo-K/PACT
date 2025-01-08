e29ab133103f592b74be558a99626befdbc5cc01
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2025-01-08 16:27:49 +0100

    style

--- html/cgu.php
+++ html/cgu.php
@@ -1,9 +1,13 @@
+<?php  require_once 'config.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Condition d'utilisation générale</title>
+  <link rel="icon" href="img/logo.png" type="image/x-icon">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <script src="js/setColor.js"></script>
