44abd42e312b4b3974408134d350bfe5beab3ccf
Matteo-K <kervadecmatteo@gmail.com>
2025-01-12 16:07:37 +0100

    test

--- html/components/cardTest.php
+++ html/components/cardTest.php
@@ -1,71 +1,70 @@
 <form action="/detailsOffer.php" method="post" class="searchA">
   <input type="hidden" name="idoffre" value="<?php echo $idOffre ?>">
-  <div class="carteOffre flip-card <?= in_array("EnRelief", $options) ? "optionEnRelief" : "" ?>">
-    <div class="flip-card-inner">
-      <figure class="flip-card-front">
-        <img src="<?= $urlImg ?>" alt="<?= $nomOffre ?>" title="<?= $nomOffre ?>">
-        <figcaption>
-          <h4 class="title"><?= $nomOffre ?></h4>
-          <div>
-            <p class="ville">
-              <?= $ville ?>
-              <span class="cardCodePostal">
-                <?= ", ".$codePostal ?>
-              </span>
-            </p>
-            <div class="blocStar">
-              <?php  
-                $etoilesPleines = floor($noteAvg); // Nombre entier d'étoiles pleines
-                $reste = $noteAvg - $etoilesPleines;
-                // Étoiles pleines
-                for ($i = 1; $i <= $etoilesPleines; $i++) { ?>
-                  <div class="star pleine"></div>
-                <?php }
-                // Étoile partielle
-                if ($reste > 0) {
-                  $pourcentageRempli = $reste * 100; // Pourcentage rempli ?>
-                  <div class="star partielle" style="--pourcentage: <?= $pourcentageRempli ?>%;"></div>
-                <?php }
-                // Étoiles vides
-                for ($i = $etoilesPleines + ($reste > 0 ? 1 : 0); $i < 5; $i++) { ?>
-                  <div class="star vide"></div>
-              <?php } ?>
-              <span>
-                <?= $noteAvg."/5" ?>
-              </span>
+  <section class="carteOffre flip-card <?= in_array("EnRelief", $options) ? "optionEnRelief" : "" ?>">
+    <!-- <article class="flip-card-inner"> -->
+      <article>
+        <figure class="flip-card-front">
+          <img src="<?= $urlImg ?>" alt="<?= $nomOffre ?>" title="<?= $nomOffre ?>">
+          <figcaption>
+            <h4 class="title"><?= $nomOffre ?></h4>
+            <div>
+              <p class="ville">
+                <?= $ville . ", ". $codePostal?>
+              </p>
+              <div class="blocStar">
+                <?php  
+                  $etoilesPleines = floor($noteAvg); // Nombre entier d'étoiles pleines
+                  $reste = $noteAvg - $etoilesPleines;
+                  // Étoiles pleines
+                  for ($i = 1; $i <= $etoilesPleines; $i++) { ?>
+                    <div class="star pleine"></div>
+                  <?php }
+                  // Étoile partielle
+                  if ($reste > 0) {
+                    $pourcentageRempli = $reste * 100; // Pourcentage rempli ?>
+                    <div class="star partielle" style="--pourcentage: <?= $pourcentageRempli ?>%;"></div>
+                  <?php }
+                  // Étoiles vides
+                  for ($i = $etoilesPleines + ($reste > 0 ? 1 : 0); $i < 5; $i++) { ?>
+                    <div class="star vide"></div>
+                <?php } ?>
+                <span>
+                  <?= $noteAvg."/5" ?>
+                </span>
+              </div>
             </div>
-          </div>
-        </figcaption>
-      </figure>
-      <div class="flip-card-back">
+          </figcaption>
+        </figure>
+      </article>
+      <article class="flip-card-back">
         <?php
-        $imageCategorie;
-        $chemin = "../img/icone/offerCategory/";
-        switch ($categorie) {
-          case 'Activité':
-            $imageCategorie = "activity.png";
-            break;
-            
-          case 'Parc Attraction':
-            $imageCategorie = "park.png";
-            break;
+          $imageCategorie;
+          $chemin = "../img/icone/offerCategory/";
+          switch ($categorie) {
+            case 'Activité':
+              $imageCategorie = "activity.png";
+              break;
+              
+            case 'Parc Attraction':
+              $imageCategorie = "park.png";
+              break;
 
-          case 'Restaurant':
-            $imageCategorie = "restaurant.png";
-            break;
+            case 'Restaurant':
+              $imageCategorie = "restaurant.png";
+              break;
 
-          case 'Spectacle':
-            $imageCategorie = "show2.png";
-            break;
+            case 'Spectacle':
+              $imageCategorie = "show2.png";
+              break;
 
-          case 'Visite':
-            $imageCategorie = "Visit.png";
-            break;
+            case 'Visite':
+              $imageCategorie = "Visit.png";
+              break;
 
-          default:
-            $imageCategorie = "interrogation.png";
-            break;
-        }
+            default:
+              $imageCategorie = "interrogation.png";
+              break;
+          }
         ?>
         <figure>
           <img src="<?= $chemin . $imageCategorie ?>" alt="<?= $categorie ?>">
@@ -73,7 +72,7 @@
             <figcaption><?= $gammeDePrix ?></figcaption>
           <?php } ?>
         </figure>
-        <div>
+        <div class="content">
           <h4 class="title"><?= $nomOffre ?></h4>
           <div class="blocStar">
               <?php  
@@ -96,19 +95,19 @@
                 <?= $noteAvg."/5" ?>
               </span>
               <span>
-                (<?= $nbNote ?> note<?= $nbNote > 1 ? "s" : "" ?>)
+                (<?= empty($nbNote) ? 0 : $nbNote ?> note<?= $nbNote > 1 ? "s" : "" ?>)
               </span>
             </div>
           </div>
           <div class="nomPro">
             Proposé par <?= $nomUser ?>
           </div>
-          <div class="content">
+          <div class="information">
             <div class="resume">
               <?= $resume ?>
             </div>
             <address>
-              <div><?= $ville . " " . $codePostal ?></div>
+              <div><?= $ville . ", " . $codePostal ?></div>
               <div><?= $numerorue . " " . $rue ?></div>
             </address>
           </div>
@@ -122,8 +121,8 @@
               }
             ?> 
           </div>
-        </div> <!-- Fermeture correcte du div de flip-card-back -->
-      </div>
-    </div>
-  </div>
+        </div>
+      </article>
+    <!-- </article> -->
+  </section>
 </form>
