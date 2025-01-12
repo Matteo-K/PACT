83486a73bb484073cb15b79be8a2cb48c5a9fd99
Matteo-K <kervadecmatteo@gmail.com>
2025-01-13 00:51:21 +0100

    appreçue erreur tags

--- html/js/sortAndFilter.js
+++ html/js/sortAndFilter.js
@@ -909,6 +909,8 @@ function createLogoCategorie(offer) {
 function ajouterTag(offer) {
   let tags = document.createElement("div");
 
+  console.log(offer.tags);
+
   if (offer.tags.length > 0) { 
     offer.tags.forEach(element => {
       
