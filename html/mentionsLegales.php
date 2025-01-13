c624871d2173c3b3fc6091cd7f767e79f4aec778
Matteo-K <kervadecmatteo@gmail.com>
2025-01-13 11:04:01 +0100

    pagination mentions legales

--- html/mentionsLegales.php
+++ html/mentionsLegales.php
@@ -12,17 +12,57 @@
 <body>
   <script src="js/setColor.js"></script>
   <?php require_once "components/header.php" ?>
-  <?php $page ?>
+  <?php 
+    $page = $_GET["page"] ?? 1;
+    $nbPage = 2;
+  ?>
   <main id="mentionsLegales">
-    <h2>
-      Données à cractère personnel
-    </h2>
-    <h2>
-      Droit d'auteur
-    </h2>
-    <h2>
-      Sécurité informatique
-    </h2>
+    <?php
+      switch ($page) {
+        case 1: ?>
+          <article>
+            <section>
+              <h2>
+                Données à cractère personnel
+              </h2>
+            </section>
+            <section>
+              <h2>
+                Droit d'auteur
+              </h2>
+            </section>
+            <section>
+              <h2>
+                Sécurité informatique
+              </h2>
+            </section>
+          </article>
+        <?php break;
+        
+        case 2: ?>
+
+          <article>
+            <h2>
+              Page 2
+            </h2>
+          </article>
+
+        <?php break;
+        
+        default:
+          
+          break;
+      }
+    ?>
+    <div class="pagination">
+      <?php if ($page > 1) { ?>
+        <a href="mentionsLegales.php?page<?= $page-1 ?>">Précédent</a>
+      <?php } ?>
+
+      <?php if ($page < $nbPage) { ?>
+        <a href="mentionsLegales.php?page<?= $page+1 ?>">Suivant</a>
+      <?php } ?>
+    </div>
   </main>
   <?php require_once "components/footer.php"; ?>
 </body>
