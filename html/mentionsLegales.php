7fc1f9f8801d7a8e28ac83076947d8265ce727ef
Matteo-K <kervadecmatteo@gmail.com>
2025-01-13 10:50:29 +0100

    page mention legale

---
+++ html/mentionsLegales.php
@@ -0,0 +1,30 @@
+<?php require_once 'config.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Mentions Legales</title>
+  <link rel="icon" href="img/logo.png" type="image/x-icon">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <script src="js/setColor.js"></script>
+  <?php require_once "components/header.php" ?>
+  <?php $page ?>
+  <main id="mentionsLegales">
+    <h2>
+      Données à cractère personnel
+    </h2>
+    <h2>
+      Droit d'auteur
+    </h2>
+    <h2>
+      Sécurité informatique
+    </h2>
+  </main>
+  <?php require_once "components/footer.php"; ?>
+</body>
+
+</html>
