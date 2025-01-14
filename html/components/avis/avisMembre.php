2117006942ed8782db6cecfa3995a9734ba74218
BZHKylian <kylian.houedec.56@gmail.com>
2025-01-14 10:18:41 +0100

    test

--- html/components/avis/avisMembre.php
+++ html/components/avis/avisMembre.php
@@ -172,6 +172,37 @@ foreach ($avis as $a) {
                 </div>
             </article>
         </div>
+        <?php
+        if ($a['idc_reponse']) {
+        ?>
+            <div>
+                <img src="./img/icone/reponse.png" alt="icone de reponse">
+                <div class="reponseAvis">
+                    <div class="user">
+                        <div class="infoProReponse">
+                            <div>
+                                <img src="<?= $result[0]['url'] ?>" alt="image de profile du pro">
+                                <p><?= ucfirst(strtolower($a['reponse_denomination'])) ?> </p>
+                            </div>
+                        </div>
+                        <div class="autreInfoAvis">
+                            <?php
+                            if (isset($a['reponsedate'])) {
+                                echo "<p>" . formatDateDiff($a["reponsedate"]) . "</p>";
+                            }
+                            ?>
+                            <img src="./img/icone/trois-points.png" alt="icone de parametre">
+                        </div>
+
+                    </div>
+                    <article>
+                        <p><?= $a['contenureponse'] ?></p>
+                    </article>
+                </div>
+            </div>
+        <?php
+        }
+        ?>
     </div>
 <?php
 }
