a142eb03addf85720acf10529e3805ecde364093
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-01-14 15:14:45 +0100

    test

--- html/index.php
+++ html/index.php
@@ -44,8 +44,8 @@
           </div>
         </div>
         
-        <div class="swiper-button-next gb"></div>
-        <div class="swiper-button-prev gb"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
         <!-- Pagination: Points -->
 
       </div>
