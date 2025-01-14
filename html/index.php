12c2061442905445607dfe253c1c47b6ed82346c
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-01-14 11:30:35 +0100

    my bad

--- html/index.php
+++ html/index.php
@@ -8,7 +8,7 @@ require_once "config.php";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <link rel="icon" href="img/logo.png" type="image/x-icon">
-  <!-- <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"> -->
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
   <title>PACT</title>
 </head>
 <body id="sansScroll">
