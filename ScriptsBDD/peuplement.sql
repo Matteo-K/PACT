dd4cbce3e11fb4481df7b484902647de64f052b9
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-01-26 22:42:47 +0100

    exemple

--- ScriptsBDD/peuplement.sql
+++ ScriptsBDD/peuplement.sql
@@ -867,6 +867,6 @@ INSERT INTO _offrePrestation_inclu(idOffre,nomPresta)
 VALUES (4,'Repas');
 
 
-
-INSERT INTO vueMessages (nomExpediteur, idExpediteur, contenuMessage, dateMessage, typeExpediteur, nomReceveur, idReceveur)
-VALUES ('membre1', 5, 'Bonjour, avez-vous un devis ?', CURRENT_TIMESTAMP, 'membre', 'Patrick', 4);
+--EXEMPLE
+-- INSERT INTO vueMessages (nomExpediteur, idExpediteur, contenuMessage, dateMessage, typeExpediteur, nomReceveur, idReceveur)
+-- VALUES ('membre1', 5, 'Bonjour, avez-vous un devis ?', CURRENT_TIMESTAMP, 'membre', 'Patrick', 4); 
