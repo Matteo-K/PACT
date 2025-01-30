9fd8a3fdc57ccbfbf4859acb5d6eeea391c80146
BZHKylian <kylian.houedec.56@gmail.com>
2025-01-30 13:54:14 +0100

    remise comme avant

--- tchatator/user.sh
+++ tchatator/user.sh
@@ -3,33 +3,11 @@
 
 # Vérifie si telnet est installé
 if ! command -v telnet &> /dev/null; then
-  sudo apt-get update && sudo apt-get install -y telnet
+  apt-get update && apt-get install -y telnet
 fi
 
 clear
 
-# Fonction pour se connecter au serveur
-connect_to_server() {
-    echo "Connexion au serveur..."
-    telnet the-void.ventsdouest.dev 8081
-    local exit_code=$?
-    
-    if [ $exit_code -ne 0 ]; then
-        echo "La connexion au serveur a été perdue."
-        return 1
-    fi
-    return 0
-}
 
-# Boucle principale
-while true; do
-    connect_to_server
-    
-    if [ $? -ne 0 ]; then
-        echo "Tentative de reconnexion dans 5 secondes..."
-        sleep 5
-    else
-        echo "Le serveur a été arrêté. Fermeture du client."
-        exit 0
-    fi
-done
+# the-void.ventsdouest.dev
+telnet the-void.ventsdouest.dev 8081
