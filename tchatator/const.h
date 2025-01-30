cf73b6b7d2ef909ac8f47bf03f1e599adae5b1c2
BZHKylian <kylian.houedec.56@gmail.com>
2025-01-30 12:16:35 +0100

    test

--- tchatator/const.h
+++ tchatator/const.h
@@ -68,7 +68,7 @@ typedef struct {
 
 // paramètre
 /// @brief Integer: port de connexion au serveur
-#define PORT 80
+#define PORT 8080
 
 /// @brief Integer: durée de ban
 extern const int DUREE_BAN;
