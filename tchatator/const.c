448b452ba530b54c5d8296b655cf4264d92f10ab
Matteo-K <kervadecmatteo@gmail.com>
2025-02-01 14:42:32 +0100

    change port

--- tchatator/const.c
+++ tchatator/const.c
@@ -8,7 +8,7 @@
 #include "const.h"
 
 // paramètre
-const int PORT = 8080;
+const int PORT = 8081;
 const int DUREE_BAN = -1;      // définitif
 const int DUREE_BLOCAGE = 86400;
 const int TAILLE_MAX_MSG = 300;
