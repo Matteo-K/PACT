4a58fa0e4cff5a14bf0617282caff892ac0e61f2
Matteo-K <kervadecmatteo@gmail.com>
2025-02-01 14:40:29 +0100

    haha

--- tchatator/tchatator.sh
+++ tchatator/tchatator.sh
@@ -4,7 +4,7 @@ clear
 
 # Compilation de tchatator
 echo "Compilation du fichier tchatator"
-if gcc -o tchatator tchatator.c const.c fonction_serveur.c bdd.c -lpq -Wall; then
+if gcc -o tchatator *.c -lpq -Wall; then
     echo "Compilation de tchatator terminée avec succès."
 else
     echo "Erreur lors de la compilation de tchatator. Arrêt du script."
