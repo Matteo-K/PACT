fe12e6349d23ace3cf68de109cf43616fded50be
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-02-02 23:14:47 +0100

    affiche

--- tchatator/fonction_serveur.c
+++ tchatator/fonction_serveur.c
@@ -243,7 +243,7 @@ void saisit_message(PGconn *conn, tClient *utilisateur, tExplodeRes requete) {
                          tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, 
                          tm.tm_hour, tm.tm_min, tm.tm_sec);
 
-                printf("%s",type_dest);
+                printf("type = %s\n",type_dest);
 
 
                 const char *ajout_message =
