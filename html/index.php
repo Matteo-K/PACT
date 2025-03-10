3dee2ea2f1579781e9704fc8297074a665e8daf2
Matteo-K <kervadecmatteo@gmail.com>
2025-03-10 11:00:28 +0100

    filtres corrections

--- html/index.php
+++ html/index.php
@@ -13,7 +13,6 @@
   <script src="js/setColor.js"></script>
 </head>
 <body id="sansScroll">
-  <?php //require_once __DIR__."/components/ecranChargement.php"; ?>
   <?php require_once "components/header.php"; ?>
   <main>
     <div id="index" class="<?= ($typeUser == "pro_public" || $typeUser == "pro_prive") ? "indexPro" : "" ?>">
