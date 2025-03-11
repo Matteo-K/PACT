da055158ceb2a4adb141250d579850053e1fd52e
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-11 09:28:10 +0100

    bizarre

--- html/components/asideTriFiltre.php
+++ html/components/asideTriFiltre.php
@@ -356,7 +356,7 @@
     
     var map = L.map('map').setView([48.8566, -2.3522], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 </script>
