e82ae062fa22f184b7f912cb7ee2f5144a3c89b3
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-11 08:47:00 +0100

    correction map trie filtre

--- html/components/asideTriFiltre.php
+++ html/components/asideTriFiltre.php
@@ -242,7 +242,7 @@
             </div>
         </aside>
         <aside id="carte_offres">
-            <article id="map">
+            <article id="mapTriFiltre">
             </article>
         </aside>
     </div>
@@ -340,10 +340,10 @@
         modal.style.display = "none";
     }
     
-    var map = L.map('map').setView([48.8566, -2.3522], 13);
+    var map = L.map('mapTriFiltre').setView([48.8566, -2.3522], 13);
 
     L.tileLayer('components/proxy.php?z={z}&x={x}&y={y}', {
-        attribution: '&copy; <a href="https://www.thunderforest.com/">Thunderforest<18e7bfc4230/a>',
+        attribution: '&copy; <a href="https://www.thunderforest.com/">Thunderforest</a>',
         maxZoom: 22
     }).addTo(map);
 </script>
