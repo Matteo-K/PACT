0ac08f7790519ac2f1f78d4d05f175c3b3f6c370
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-11 14:16:36 +0100

    ajout de type module dans l'import du script sortAndFilter

--- html/index.php
+++ html/index.php
@@ -149,7 +149,7 @@
     <!-- Data -->
     <div id="offers-data" data-offers='<?php echo htmlspecialchars(json_encode($offres->getArray($offres->filtre($idUser, $typeUser)))); ?>'></div>
     <div id="user-data" data-user='<?php echo $typeUser ?>'></div>
-    <script src="js/sortAndFilter.js"></script>
+    <script type="module" src="js/sortAndFilter.js"></script>
     <?php require_once "components/footer.php"; ?>
     <script>
       const forms = document.querySelectorAll("#index form");
