ce25d6084545ca3a94a04780440193143d0d2ed3
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-11 10:53:47 +0100

    autre correcton  enlevement du setTimeout pour la carte qui a ete mis dans un evennement js

--- html/components/asideTriFiltre.php
+++ html/components/asideTriFiltre.php
@@ -362,9 +362,6 @@
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; CartoDB'
     }).addTo(map);
-    setTimeout(() => {
-        map.invalidateSize();
-    }, 500);
 
     document.getElementById('btnCarte').addEventListener('click', function() {
         // Attendre un court instant avant de recalculer la taille
