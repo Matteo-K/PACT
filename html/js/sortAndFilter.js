2a98995c4aeb560669cbf9683b7c90708fdc43eb
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-12 16:04:02 +0100

    test

--- html/js/sortAndFilter.js
+++ html/js/sortAndFilter.js
@@ -649,7 +649,7 @@ function addPing(array) {
                     <div>
                       ${displayStar(parseFloat(elt["noteAvg"])).outerHTML}
                       <p>
-                      ${elt["noteAvg"]}
+                        ${elt["noteAvg"]}
                       </p>
                     </div>
                     <p><strong>Résumé :</strong> ${elt['resume']}</p>
