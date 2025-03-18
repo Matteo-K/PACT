be67574f949b228c3019e8280c49d623a9396dcc
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-18 16:44:27 +0100

    test

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -1559,15 +1559,19 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
         });
     </script>
     <script type="module">
-        import { geocode } from "./js/geocode.js";
+        import {
+            geocode
+        } from "./js/geocode.js";
         try {
-            let marker; // Variable pour stocker le marqueur actuel
-
-            // Initialisation de la carte Google
             var map = L.map('map').setView([51.505, -0.09], 13);
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png}', {
-                maxZoom: 22
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
+
+            L.marker([51.5, -0.09]).addTo(map)
+                .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+                .openPopup();
 
         } catch (error) {
 
