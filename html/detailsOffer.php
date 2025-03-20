a7a119588e540c6a2d73c9e73516a61c3cf952f2
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-20 09:12:54 +0100

    test

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -1571,15 +1571,13 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
             ?>
             let address = <?php echo json_encode($result[0]["numerorue"] . " " . $result[0]["rue"] . ", " . $result[0]["codepostal"] . " " . $result[0]["ville"]); ?>;
             // Assuming geocode() returns a promise with latitude and longitude
+            let map = L.map('map').setView([48.46, -2.85], 15);
+
             geocode(address)
                 .then(location => {
                     const latLng = location;
                     if (latLng) {
-                        let map = L.map('map').setView(latLng, 15);
                         let marker = L.marker(latLng)  
-                    }
-                    else{
-                        let map = L.map('map').setView([48.46, -2.85], 15);
                     }
                 })
                 .catch(error => {
