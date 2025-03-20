b0e07a03cd55a307becf6f1cf2a0dbdd2c9723b2
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-03-20 12:25:23 +0100

    ok erreur pdo

--- html/blacklist.php
+++ html/blacklist.php
@@ -6,8 +6,8 @@ $donnees = json_decode(file_get_contents('php://input'), true);
 $idAvis = $donnees['idC'];
 $idOffre = $donnees['idOffre'];
 
-$stmt = $conn->prepare("SELECT * FROM pact._avis WHERE idc = ");
-$stmt->execute();
+$stmt = $conn->prepare("SELECT * FROM pact._avis WHERE idc = ?");
+$stmt->execute([$idAvis]);
 
 
 if (!($stmt->fetch())['blacklist']) {
@@ -41,8 +41,8 @@ if (!($stmt->fetch())['blacklist']) {
 
     $stmt->execute([$idAvis, $idOffre]);
 
-    // $stmt = $conn->prepare("UPDATE pact._avis set blacklist = true WHERE idc = ?");
-    // $stmt->execute([$idAvis]);
+    $stmt = $conn->prepare("UPDATE pact._avis set blacklist = true WHERE idc = ?");
+    $stmt->execute([$idAvis]);
 
     http_response_code(200);
 } else {
