9dca8ab433e1ad630c4f55a3a40bb8e576ff0013
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-03-21 11:51:53 +0100

    ??

--- html/authentikator/validation_2fa.php
+++ html/authentikator/validation_2fa.php
@@ -17,13 +17,21 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['code_2fa'])) {
     $secret = $_SESSION['secret_2fa'];
     $totp = TOTP::create($secret);
 
+    echo "<pre>";
+    echo "Code entré : " . $code . "<br>";
+    echo "Secret en session : " . $secret . "<br>";
+    echo "Code actuel (expected) : " . $totp->now() . "<br>";
+    echo "</pre>";
+
+    // Vérifier si le code est valide avec une fenêtre de 1 période (30 secondes)
     // Permet de vérifier le code actuel et celui des 30 secondes précédentes
-    if ($totp->verify($code, null, 2)) { 
+    if ($totp->verify($code, null, 2)) {  // Test avec window = 2 (actuel et une période avant)
         $_SESSION['2fa_verified'] = true;
         echo "<span style='color: green;'>2FA activé avec succès !</span>";
     } else {
         $_SESSION['2fa_verified'] = false;
         echo "<span style='color: red;'>Code invalide. Réessayez.</span>";
     }
+}
 }
 ?>
