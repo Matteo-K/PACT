05bc00b2f1205e7896cb63c5ff182d56265583b9
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-03-25 11:45:37 +0100

    ok test

--- html/changeAccountMember.php
+++ html/changeAccountMember.php
@@ -15,6 +15,8 @@
     $userId = $_SESSION['idUser'];
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        print_r($_POST);
+        print_r($_SESSION);
         if (isset($_SESSION["a2f_verifier"]) && $_POST["authentikator"] == "on" && strlen($_POST["code_2fa"] == 6)) {
             $stmt = $conn->prepare("UPDATE pact._utilisateur set secret_a2f = ? , confirm_a2f = ? WHERE idu = ?");
             $stmt->execute([$_POST["secret_a2f"],true,$userId]);
