06528ec1949a9e1db9d7e57d7ea6a513382b8c6d
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2025-03-25 11:14:34 +0100

    jespere ca marche sinon clc

--- html/components/header.php
+++ html/components/header.php
@@ -4,7 +4,15 @@
         <a href="../index.php">
             <img id="logo" src="../img/logo.png" title="logo du site">
             <div>
-                <h1 id="logoText">PACT</h1>
+                <?php
+                if ($isLoggedIn) {
+                    if ($_SESSION["typeUser"] != "pro_public" || $_SESSION["typeUser"] != "pro_prive") {
+                ?>
+                        <h1 id="pro">PACT PRO</h1>
+                <?php
+                    }
+                }
+                ?>
                 <?php
                 if ($isLoggedIn) {
                     if ($_SESSION["typeUser"] == "pro_public" || $_SESSION["typeUser"] === "pro_prive") {
