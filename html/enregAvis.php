c5a42369dd83d77c90f1357264d80a96ab2b882f
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-03-25 12:26:00 +0100

    ok

--- html/enregAvis.php
+++ html/enregAvis.php
@@ -162,8 +162,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt = $conn->prepare("DELETE FROM pact._commentaire WHERE idc = $idAvis");
         $stmt -> execute();
 
-        $stmt = $conn->prepare("DELETE FROM pact._blacklist WHERE idc = $idAvis");
+        $stmt = $conn->prepare("SELECT * FROM pact._blacklist WHERE idc = $idAvis");
         $stmt -> execute();
+
+        if ($stmt->fetch()) {
+            $stmt = $conn->prepare("DELETE FROM pact._blacklist WHERE idc = $idAvis");
+            $stmt -> execute();
+        }
+
+        
 
     } elseif($_POST["action"] == "supprimerReponse"){
         $idAvis = $_POST['id'] ?? null;
