b5696c24cd9fe619546b3943c294c369c94c4a6a
Benjamin <benjamin.girard.1@etudiant.univ-rennes1.fr>
2025-03-25 11:23:53 +0100

    presque

--- html/components/header.php
+++ html/components/header.php
@@ -9,7 +9,7 @@
                 if ($isLoggedIn) {
                     if ($_SESSION["typeUser"] !== "pro_public" || $_SESSION["typeUser"] !== "pro_prive") {
                 ?>
-                <h1 id="logoText">PACT</h1>
+                <h1 id="logoText">PACT PRO</h1>
                 <?php
                     }
                 }
