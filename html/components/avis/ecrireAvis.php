34133fadbf648abd9dfc79ef67d32f286d6ee10b
BZHKylian <kylian.houedec.56@gmail.com>
2025-03-27 20:08:03 +0100

    correction car mal orthographier le getElementById("date");

--- html/components/avis/ecrireAvis.php
+++ html/components/avis/ecrireAvis.php
@@ -181,7 +181,7 @@
             });
         }
 
-        const date = document.getElementsById('date-avis');
+        const date = document.getElementById('date-avis');
         const radios = document.getElementsByName('compagnie');
         const titre = document.getElementById("titre");
         const avis = document.getElementById("avis");
