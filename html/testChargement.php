18cf0bf252eb099ab0303bce9e0de24c0014356f
ejain6 <ewen.jain@gmail.com>
2025-03-28 15:49:05 +0100

    chargement

--- html/testChargement.php
+++ html/testChargement.php
@@ -6,17 +6,15 @@
     <link rel="stylesheet" href="style.css">
     <title>PACT - Chargement...</title>
 </head>
-<body id="#chargement">
+<body id="chargement">
 
     <div>
         <img id="logo" src="../img/logo.png" title="logo du site" class="chargementActif">
         <h1 id="logoText">PACT</h1>
     </div>
-    <section>
-        <p>
-            Texte textuel bientôt différent du texte actuel
-        </p>
-    </section>
+    <p>
+        Texte textuel bientôt différent du texte actuel
+    </p>
 
 
     <script>
