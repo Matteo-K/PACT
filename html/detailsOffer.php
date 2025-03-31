683f1d309fe5db6b58f6f1adeb1b1b83c392fac7
Gabriel Froc <gabrielfroc@etudiants.univ-rennes1.fr>
2025-03-31 15:24:57 +0200

    oups

--- html/detailsOffer.php
+++ html/detailsOffer.php
@@ -1767,7 +1767,6 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         }
                     
                         let avis = data.notes || [];
-                        displayArray
 
                         divs.forEach(div => {
                             div.innerHTML = "";
